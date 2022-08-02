044233b4972df99c6f63668487e7f3b0c177fc6d
Yanfeng Zhao <zhaoyanfeng@google.com>
2022-08-02 00:11:16 -0700

    Internal change
    PiperOrigin-RevId: 464717198

---
+++ src/java/com/google/devtools/mobileharness/api/model/error/MobileHarnessException.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.mobileharness.api.model.error;
+
+import com.google.devtools.common.metrics.stability.model.ErrorIdProvider;
+import com.google.devtools.deviceinfra.api.error.DeviceInfraException;
+import com.google.wireless.qa.mobileharness.shared.constant.ErrorCode;
+import javax.annotation.Nullable;
+
+/**
+ * Base class of all Mobile Harness exceptions.
+ *
+ * @see <a href="http://go/mh-exception">MH Exception</a> for more detail and guidance
+ */
+public class MobileHarnessException
+    extends com.google.wireless.qa.mobileharness.shared.MobileHarnessException
+    implements ErrorIdProvider<ErrorId> {
+
+  private final ErrorId errorId;
+
+  public MobileHarnessException(ErrorId errorId, String message) {
+    this(errorId, message, /* cause= */ null);
+  }
+
+  public MobileHarnessException(ErrorId errorId, String message, @Nullable Throwable cause) {
+    this(errorId, message, cause, !message.endsWith(getMessageSuffix(errorId)));
+  }
+
+  public MobileHarnessException(
+      ErrorId errorId, String message, @Nullable Throwable cause, boolean addErrorIdToMessage) {
+    super(
+        ErrorCode.NEXT_GEN_ERROR,
+        ErrorType.UNCLASSIFIED_ERROR,
+        addErrorIdToMessage ? message + getMessageSuffix(errorId) : message,
+        cause,
+        false /* don't add cause to message */);
+    this.errorId = errorId;
+  }
+
+  @Override
+  public ErrorId getErrorId() {
+    return errorId;
+  }
+
+  public static <T> T checkNotNull(T reference, ErrorId errorId, @Nullable Object message)
+      throws MobileHarnessException {
+    if (reference == null) {
+      throw new MobileHarnessException(errorId, String.valueOf(message));
+    }
+    return reference;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @deprecated Please use {@link #getErrorId()}.code()
+   */
+  @Override
+  @Deprecated
+  public int getErrorCode() {
+    return errorId.code();
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>@deprecated Please use {@link #getErrorId()}.name()
+   */
+  @Override
+  @Deprecated
+  public String getErrorName() {
+    return errorId.name();
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>@deprecated Please use {@link #getErrorId()}.type()
+   */
+  @Override
+  @Deprecated
+  public ErrorType getErrorType() {
+    switch (errorId.type()) {
+      case INFRA_ISSUE:
+      case DEPENDENCY_ISSUE:
+        return ErrorType.INFRA_ERROR;
+      case CUSTOMER_ISSUE:
+        return ErrorType.USERS_FAILURE;
+      case UNCLASSIFIED:
+      case UNDETERMINED:
+      default:
+        return ErrorType.UNCLASSIFIED_ERROR;
+    }
+  }
+
+  /** Converts an old {@link MobileHarnessException} to a new {@link DeviceInfraException}. */
+  public DeviceInfraException toNewException() {
+    DeviceInfraException result =
+        new DeviceInfraException(getErrorId(), getMessage(), /* addErrorIdToMessage= */ false);
+    result.setStackTrace(getStackTrace());
+    Throwable cause = getCause();
+    if (cause != null) {
+      result.initCause(toNewExceptionIfPossible(cause));
+    }
+    for (Throwable suppressed : getSuppressed()) {
+      result.addSuppressed(toNewExceptionIfPossible(suppressed));
+    }
+    return result;
+  }
+
+  private static Throwable toNewExceptionIfPossible(Throwable exception) {
+    return exception instanceof MobileHarnessException
+        ? ((MobileHarnessException) exception).toNewException()
+        : exception;
+  }
+
+  private static String getMessageSuffix(ErrorId errorId) {
+    return " " + errorId;
+  }
+}
---
+++ src/java/com/google/devtools/mobileharness/api/model/error/MobileHarnessExceptions.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.mobileharness.api.model.error;
+
+import com.google.common.base.Throwables;
+import java.util.function.Supplier;
+import javax.annotation.Nullable;
+
+/**
+ * Utilities for {@link MobileHarnessException}.
+ *
+ * @see <a href="http://go/mh-exception">MH Exception</a> for more detail and guidance
+ */
+public class MobileHarnessExceptions {
+
+  /**
+   * @see com.google.common.base.Preconditions#checkArgument
+   * @see com.google.common.base.Preconditions#checkState
+   */
+  public static void check(boolean expression, ErrorId errorId, Supplier<String> errorMessage)
+      throws MobileHarnessException {
+    if (!expression) {
+      throw new MobileHarnessException(errorId, errorMessage.get());
+    }
+  }
+
+  /**
+   * Rethrows the given throwable as a more specific Mobile Harness exception.
+   *
+   * <p>This method will never return so its type parameter is unused.
+   *
+   * @param throwable a throwable which is actually a {@link MobileHarnessException}/{@link
+   *     InterruptedException}/{@link RuntimeException}/{@link Error}
+   * @param defaultError the default error to throw if the throwable is an unexpected non-MH checked
+   *     exception or {@code null}
+   * @throws MobileHarnessException if the throwable is actually a {@link MobileHarnessException} or
+   *     the throwable is an unexpected non-MH checked exception or {@code null}
+   * @throws InterruptedException if the throwable is actually an {@link InterruptedException}
+   * @throws RuntimeException if the throwable is actually a {@link RuntimeException}
+   * @throws Error if the throwable is actually an {@link Error}
+   */
+  @SuppressWarnings("TypeParameterUnusedInFormals")
+  public static <T> T rethrow(@Nullable Throwable throwable, ErrorId defaultError)
+      throws MobileHarnessException, InterruptedException {
+    Throwables.propagateIfPossible(
+        throwable, MobileHarnessException.class, InterruptedException.class);
+    throw new MobileHarnessException(
+        defaultError,
+        String.format(
+            "Unexpected non-MH checked exception [%s]",
+            throwable == null ? null : throwable.getClass().getName()),
+        throwable);
+  }
+
+  private MobileHarnessExceptions() {}
+}
