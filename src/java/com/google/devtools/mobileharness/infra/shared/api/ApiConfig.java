a9bff321323ec46ee88120de015f8bea5c9c92ab
Yanfeng Zhao <zhaoyanfeng@google.com>
2022-10-08 23:41:06 -0700

    Internal change
    PiperOrigin-RevId: 479872807

---
+++ src/java/com/google/devtools/mobileharness/infra/shared/api/ApiConfig.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.mobileharness.infra.shared.api;
+
+import com.google.devtools.mobileharness.api.deviceconfig.proto.Basic.WifiConfig;
+import com.google.protobuf.Any;
+import com.google.wireless.qa.mobileharness.shared.MobileHarnessException;
+import com.google.wireless.qa.mobileharness.shared.proto.Common.StrPair;
+import java.util.List;
+import java.util.Map;
+import java.util.Observer;
+import java.util.Optional;
+
+/** The class to operate lab and device config. */
+public interface ApiConfig {
+
+  static ApiConfig getInstance() {
+     return DummyApiConfig.getInstance();
+  }
+
+  /** Initialize. */
+  void init(boolean isLocalMode) throws MobileHarnessException;
+
+  /** Gets all the monitored device ids. */
+  List<String> getMonitoredDeviceUuids();
+
+  /** Gets all the monitored device ids. */
+  List<String> getOverTcpDeviceControlIds();
+
+  /** Gets the max failed consecutive test count for the given driver. */
+  int getMaxConsecutiveFail(String deviceControlId);
+
+  /** Gets the max consecutive test count for the device in the lab. */
+  int getMaxConsecutiveTest(String deviceControlId);
+
+  /** Gets the owners of the given device. */
+  List<String> getOwners(String deviceControlId);
+
+  /** Gets the executors of the given device. */
+  List<String> getExecutors(String deviceControlId);
+
+  /** Gets the customized config of the given device. */
+  Optional<Any> getCustomizedConfig(String deviceControlId);
+
+  /** Get the supported dimensions of the given device. */
+  List<StrPair> getSupportedDimensions(String deviceControlId);
+
+  /** Gets the required dimensions of the given device. */
+  List<StrPair> getRequiredDimensions(String deviceControlId);
+
+  /** Gets all over ssh devices info. */
+  Map<String, List<String>> getOverSshDevice();
+
+  /** Gets the wifi config for the given device. */
+  WifiConfig getDefaultWifi(String deviceControlId);
+
+  /** Adds the observer to listen to config change. */
+  void addObserver(Observer observer);
+}
