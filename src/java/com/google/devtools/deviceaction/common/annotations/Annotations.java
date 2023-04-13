ffed9b83a40d73f73c646934a4b8c5ef3bd66a92
DeviceInfra <no-reply@google.com>
2023-04-13 05:04:20 -0700

    Internal change
    PiperOrigin-RevId: 523970350

---
+++ src/java/com/google/devtools/deviceaction/common/annotations/Annotations.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.deviceaction.common.annotations;
+
+import com.google.protobuf.Message;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/** Annotations for code processing. */
+public final class Annotations {
+  private Annotations() {}
+
+  /** Annotation for classes configured by spec proto. */
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target(ElementType.TYPE)
+  public @interface Configurable {
+
+    Class<? extends Message> specType();
+  }
+
+  /** Annotation for methods to return spec values. */
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target(ElementType.METHOD)
+  public @interface SpecValue {
+    String field();
+  }
+}
