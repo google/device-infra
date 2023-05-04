65fef340edc53d8c07d6e28c0c675dc4df22d914
Shiheng Wang <shiheng@google.com>
2023-05-03 20:42:16 -0700

    Create configuration util to handle configuration files.
    PiperOrigin-RevId: 529273971

---
+++ src/java/com/google/devtools/xts/config/ConfigurationUtil.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.xts.config;
+
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static java.util.Arrays.stream;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.flogger.FluentLogger;
+import com.google.devtools.mobileharness.api.model.error.MobileHarnessException;
+import com.google.devtools.mobileharness.shared.util.file.local.LocalFileUtil;
+import java.io.File;
+import java.io.FileFilter;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.annotation.Nullable;
+
+/** Utility functions to handle configuration files. */
+final class ConfigurationUtil {
+
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private static final String[] CONFIG_FILE_EXTENSIONS = {".config", ".xml"};
+
+  private final LocalFileUtil localFileUtil;
+
+  public ConfigurationUtil() {
+    this.localFileUtil = new LocalFileUtil();
+  }
+
+  /**
+   * Gets the test config file paths from given directories.
+   *
+   * @param subPath subdirectory to look for configurations. If null, only config files from {@code
+   *     dirs} will be searched.
+   * @param dirs a list of {@link File} of extra directories to search for test configurations
+   */
+  public ImmutableSet<String> getConfigPathsFromDirs(@Nullable String subPath, List<File> dirs) {
+    return getConfigFilesFromDirs(subPath, dirs).stream()
+        .map(File::getAbsolutePath)
+        .collect(toImmutableSet());
+  }
+
+  /**
+   * Gets the test config files from given directories.
+   *
+   * @param subPath subdirectory to look for configurations
+   * @param dirs A list of {@link File} of extra directories to search for test configs
+   * @return the set of {@link File} that were found.
+   */
+  public Set<File> getConfigFilesFromDirs(@Nullable String subPath, List<File> dirs) {
+    FileFilter configFileFilter =
+        (file) -> {
+          return stream(CONFIG_FILE_EXTENSIONS)
+              .anyMatch(extension -> file.getName().endsWith(extension));
+        };
+
+    return getConfigFilesFromDirs(subPath, dirs, configFileFilter);
+  }
+
+  /**
+   * Search a particular pattern of in the given directories.
+   *
+   * @param subPath subdirectory to look for configurations
+   * @param dirs A list of {@link File} of extra directories to search for test configs
+   * @param configFileFilter filters out configuration files
+   * @return the set of {@link File} that were found
+   */
+  public Set<File> getConfigFilesFromDirs(
+      @Nullable String subPath, List<File> dirs, FileFilter configFileFilter) {
+    Set<File> configNames = new LinkedHashSet<>();
+    for (File dir : dirs) {
+      File configDir = new File(dir.getAbsolutePath());
+      if (subPath != null) {
+        configDir = new File(dir, subPath);
+      }
+      if (!configDir.isDirectory()) {
+        logger.atWarning().log(
+            "%s doesn't exist or is not a directory.", configDir.getAbsolutePath());
+        continue;
+      }
+      try {
+        configNames.addAll(
+            localFileUtil.listFiles(
+                configDir.getAbsolutePath(), /* recursively= */ false, configFileFilter));
+      } catch (MobileHarnessException e) {
+        logger.atSevere().withCause(e).log(
+            "Failed to get test config files from directory %s", configDir.getAbsolutePath());
+      }
+    }
+    // From a same tests dir we only expect a single instance of each names, so we dedup the files
+    // if that happens.
+    return dedupFiles(configNames);
+  }
+
+  /** Filters out the configuration files and depups the files having the same names. */
+  private Set<File> dedupFiles(Set<File> originalConfigSet) {
+    Map<String, File> configNameToFileMap = new LinkedHashMap<>();
+    for (File file : originalConfigSet) {
+      try {
+        /*
+         */
+        if (!localFileUtil.readFile(file.getAbsolutePath()).contains("<configuration")) {
+          logger.atInfo().log("%s doesn't look like a test configuration.", file.getAbsolutePath());
+          continue;
+        }
+      } catch (MobileHarnessException e) {
+        logger.atSevere().withCause(e).log(
+            "Failed to check file content %s", file.getAbsolutePath());
+        continue;
+      }
+      // Always keep the first found
+      configNameToFileMap.putIfAbsent(file.getName(), file);
+    }
+    return new LinkedHashSet<>(configNameToFileMap.values());
+  }
+}
---
+++ src/javatests/com/google/devtools/xts/config/ConfigurationUtilTest.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.xts.config;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.devtools.mobileharness.shared.util.file.local.LocalFileUtil;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Unit tests for {@link ConfigurationUtil}. */
+@RunWith(JUnit4.class)
+public class ConfigurationUtilTest {
+
+  @Rule public TemporaryFolder tempFolder = new TemporaryFolder();
+
+  private Path tempDirPath;
+
+  private ConfigurationUtil configurationUtil;
+  private LocalFileUtil localFileUtil;
+
+  @Before
+  public void setUpConfigurationUtil() throws Exception {
+    configurationUtil = new ConfigurationUtil();
+    localFileUtil = new LocalFileUtil();
+  }
+
+  @Before
+  public void prepareTempDir() throws Exception {
+    tempDirPath = tempFolder.getRoot().toPath();
+  }
+
+  @Test
+  public void getConfigPathsFromDirs_configContent_returnsConfigNames() throws Exception {
+    Path testcasesDir = Paths.get(tempDirPath.toString(), "testcases");
+    Path newFile1 = testcasesDir.resolve("new_file1.txt");
+    Path newFile2 = testcasesDir.resolve("new_file2.config");
+    Path newFile3 = testcasesDir.resolve("new_file3.xml");
+    localFileUtil.writeToFile(newFile1.toString(), "<configuration>");
+    localFileUtil.writeToFile(newFile2.toString(), "<configuration>");
+    localFileUtil.writeToFile(newFile3.toString(), "<configuration>");
+
+    ImmutableSet<String> configNames =
+        configurationUtil.getConfigPathsFromDirs(
+            "testcases", ImmutableList.of(tempDirPath.toFile()));
+
+    assertThat(configNames).containsExactly(newFile2.toString(), newFile3.toString());
+  }
+
+  @Test
+  public void getConfigPathsFromDirs_nonConfigContent_returnsEmptyList() throws Exception {
+    Path testcasesDir = Paths.get(tempDirPath.toString(), "testcases");
+    Path newFile1 = testcasesDir.resolve("new_file1.txt");
+    Path newFile2 = testcasesDir.resolve("new_file2.config");
+    Path newFile3 = testcasesDir.resolve("new_file3.xml");
+    localFileUtil.writeToFile(newFile1.toString(), "<not_configuration>");
+    localFileUtil.writeToFile(newFile2.toString(), "<not_configuration>");
+    localFileUtil.writeToFile(newFile3.toString(), "<not_configuration>");
+
+    ImmutableSet<String> configNames =
+        configurationUtil.getConfigPathsFromDirs(
+            "testcases", ImmutableList.of(tempDirPath.toFile()));
+
+    assertThat(configNames).isEmpty();
+  }
+
+  @Test
+  public void getConfigPathsFromDirs_multiDir_returnsConfigNames() throws Exception {
+    Path testcasesDir1 = Paths.get(tempDirPath.toString(), "cts1");
+    Path testcasesDir2 = Paths.get(tempDirPath.toString(), "cts2");
+    Path newFile11 = testcasesDir1.resolve("testcases").resolve("new_file1.txt");
+    Path newFile12 = testcasesDir1.resolve("testcases").resolve("new_file2.config");
+    Path newFile21 = testcasesDir2.resolve("testcases").resolve("new_file3.xml");
+    localFileUtil.writeToFile(newFile11.toString(), "<configuration>");
+    localFileUtil.writeToFile(newFile12.toString(), "<configuration>");
+    localFileUtil.writeToFile(newFile21.toString(), "<configuration>");
+
+    ImmutableSet<String> configNames =
+        configurationUtil.getConfigPathsFromDirs(
+            "testcases", ImmutableList.of(testcasesDir1.toFile(), testcasesDir2.toFile()));
+
+    assertThat(configNames).containsExactly(newFile12.toString(), newFile21.toString());
+  }
+
+  @Test
+  public void getConfigPathsFromDirs_duplicateModuleName_dedupConfigNames() throws Exception {
+    Path testcasesDir1 = Paths.get(tempDirPath.toString(), "cts1");
+    Path testcasesDir2 = Paths.get(tempDirPath.toString(), "cts2");
+    Path newFile11 = testcasesDir1.resolve("testcases").resolve("new_file1.txt");
+    Path newFile12 = testcasesDir1.resolve("testcases").resolve("new_file2.xml");
+    Path newFile21 = testcasesDir2.resolve("testcases").resolve("new_file2.xml");
+    localFileUtil.writeToFile(newFile11.toString(), "<configuration>");
+    localFileUtil.writeToFile(newFile12.toString(), "<configuration>");
+    localFileUtil.writeToFile(newFile21.toString(), "<configuration>");
+
+    ImmutableSet<String> configNames =
+        configurationUtil.getConfigPathsFromDirs(
+            "testcases", ImmutableList.of(testcasesDir1.toFile(), testcasesDir2.toFile()));
+
+    assertThat(configNames).containsExactly(newFile12.toString());
+  }
+}
