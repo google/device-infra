22d4f98b057f18b328f507ee5e4ce59a85eeb519
Shiheng Wang <shiheng@google.com>
2023-05-05 23:19:14 -0700

    refactor. Move the xts code to a new package.
    PiperOrigin-RevId: 529906526

--- src/java/com/google/devtools/mobileharness/platform/android/xts/config/ConfigurationUtil.java
+++ src/java/com/google/devtools/mobileharness/platform/android/xts/config/ConfigurationUtil.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.google.devtools.xts.config;
+package com.google.devtools.mobileharness.platform.android.xts.config;
 
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static java.util.Arrays.stream;
