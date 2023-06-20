1d98735c3491a1dbc76cccad8b55e7767b3d27c2
Shian Hou <shian@google.com>
2023-06-19 20:57:06 -0700

    Add integration test to list results command of ATS console.
    PiperOrigin-RevId: 541774242

--- src/javatests/com/google/devtools/atsconsole/command/ListCommandTest.java
+++ src/javatests/com/google/devtools/atsconsole/command/ListCommandTest.java
@@ -32,8 +32,10 @@ import com.google.common.truth.Correspondence.BinaryPredicate;
 import com.google.devtools.atsconsole.AtsConsole;
 import com.google.devtools.atsconsole.AtsConsoleModule;
 import com.google.devtools.deviceinfra.shared.util.flags.Flags;
+import com.google.devtools.deviceinfra.shared.util.path.PathUtil;
 import com.google.devtools.deviceinfra.shared.util.port.PortProber;
 import com.google.devtools.deviceinfra.shared.util.runfiles.RunfilesUtil;
+import com.google.devtools.mobileharness.shared.util.file.local.LocalFileUtil;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import java.io.ByteArrayOutputStream;
@@ -69,6 +71,7 @@ public class ListCommandTest {
   @Mock private LineReader lineReader;
 
   private String publicDirPath;
+  private String xtsRootDirPath;
 
   private ByteArrayOutputStream consoleOutOutputStream;
   private ByteArrayOutputStream consoleErrOutputStream;
@@ -80,6 +83,7 @@ public class ListCommandTest {
     // Prepares environment.
     int olcServerPort = PortProber.pickUnusedPort();
     publicDirPath = tmpFolder.newFolder("public_dir").toString();
+    xtsRootDirPath = tmpFolder.newFolder("xts_root_dir").toString();
     Path olcServerBinary =
         Path.of(
             RunfilesUtil.getRunfilesLocation(
@@ -116,7 +120,7 @@ public class ListCommandTest {
             new AtsConsoleModule(
                 deviceInfraServiceFlags,
                 /* mainArgs= */ ImmutableList.of(),
-                /* systemProperties= */ ImmutableMap.of(),
+                /* systemProperties= */ ImmutableMap.of("XTS_ROOT", xtsRootDirPath),
                 lineReader,
                 consoleOutPrintStream,
                 consoleErrPrintStream,
@@ -134,21 +138,26 @@ public class ListCommandTest {
     System.out.println("ATS console stderr:");
     System.out.println(consoleErrOutputStream.toString(UTF_8));
 
-    try (Stream<Path> files = Files.list(Path.of(publicDirPath, "olc_server_log"))) {
-      files.forEach(
-          path -> {
-            try {
-              String fileContent = Files.readString(path);
-              logger.atInfo().log(
-                  "OLC server log file [%s]:\n"
-                      + "**BEGIN******************\n"
-                      + "%s\n"
-                      + "**END******************\n",
-                  path, fileContent);
-            } catch (IOException e) {
-              logger.atWarning().withCause(e).log("Failed to read %s", path);
-            }
-          });
+    Path serverLogDir = Path.of(publicDirPath, "olc_server_log");
+    if (Files.exists(serverLogDir)) {
+      try (Stream<Path> files = Files.list(serverLogDir)) {
+        files.forEach(
+            path -> {
+              if (!Files.isDirectory(path)) {
+                try {
+                  String fileContent = Files.readString(path);
+                  logger.atInfo().log(
+                      "OLC server log file [%s]:\n"
+                          + "**BEGIN******************\n"
+                          + "%s\n"
+                          + "**END******************\n",
+                      path, fileContent);
+                } catch (IOException e) {
+                  logger.atWarning().withCause(e).log("Failed to read %s", path);
+                }
+              }
+            });
+      }
     }
   }
 
@@ -174,5 +183,29 @@ public class ListCommandTest {
         .containsAtLeast("Serial", "NoOpDevice-0", "Serial", "NoOpDevice-0", "")
         .inOrder();
     assertThat(stdout).contains(String.join("\n", CTS_MODULE_LIST));
+  }
+
+  @Test
+  public void listResults() throws Exception {
+    String resultFileOriginalPath =
+        RunfilesUtil.getRunfilesLocation(
+            "javatests/com/google/devtools/atsconsole/command/testdata/test_result.xml");
+    String resultFilePath =
+        PathUtil.join(
+            xtsRootDirPath, "android-cts", "results", "2023.11.30_12.34.56", "test_result.xml");
+    LocalFileUtil localFileUtil = new LocalFileUtil();
+    localFileUtil.prepareParentDir(resultFilePath);
+    localFileUtil.copyFileOrDir(resultFileOriginalPath, resultFilePath);
+
+    when(lineReader.readLine(anyString())).thenReturn("list results").thenReturn("exit");
+
+    atsConsole.call();
+
+    assertThat(consoleOutOutputStream.toString(UTF_8))
+        .isEqualTo(
+            "Session  Pass  Fail  Modules Complete  Result Directory     Test Plan  Device"
+                + " serial(s)  Build ID            Product\n"
+                + "0        117   0     1 of 1            2023.11.30_12.34.56  cts        ABC, DEF "
+                + "         SQ3A.220705.003.A1  redfin\n");
   }
 }
