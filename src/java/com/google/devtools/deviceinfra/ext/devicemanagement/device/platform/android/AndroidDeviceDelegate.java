6bfa407c1483a96021afa6293c2a01d45493c9e7
DeviceInfra <no-reply@google.com>
2023-09-25 14:15:42 -0700

    Internal change
    PiperOrigin-RevId: 568321897

--- src/java/com/google/devtools/deviceinfra/ext/devicemanagement/device/platform/android/AndroidDeviceDelegate.java
+++ src/java/com/google/devtools/deviceinfra/ext/devicemanagement/device/platform/android/AndroidDeviceDelegate.java
@@ -593,6 +593,7 @@ public abstract class AndroidDeviceDelegate {
     device.addSupportedDecorator("AndroidSystemHealthMemoryDecorator");
     device.addSupportedDecorator("AndroidPerfettoDecorator");
     device.addSupportedDecorator("AndroidWebviewBridgeDecorator");
+    device.addSupportedDecorator("DimensionTestArgsDecorator");
     device.addSupportedDecorator("ManekiYouTubeLauncherDecorator");
     device.addSupportedDecorator("ManekiAndroidWebdriverPortForwardDecorator");
     device.addSupportedDecorator("MaskImageDecorator");
