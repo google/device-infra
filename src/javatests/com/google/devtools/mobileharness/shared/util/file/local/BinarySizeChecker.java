6e94367e98efb4ad1c7c904a0aae6833af84f24b
Shian Hou <shian@google.com>
2023-12-25 01:12:24 -0800

    Internal change
    PiperOrigin-RevId: 593560918

--- src/javatests/com/google/devtools/mobileharness/shared/util/file/local/BinarySizeChecker.java
+++ src/javatests/com/google/devtools/mobileharness/shared/util/file/local/BinarySizeChecker.java
@@ -16,23 +16,86 @@
 
 package com.google.devtools.mobileharness.shared.util.file.local;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.google.common.truth.Truth.assertWithMessage;
+import static java.util.Comparator.comparingLong;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.devtools.mobileharness.api.model.error.MobileHarnessException;
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
 
 /** Checker for checking binary sizes. */
 public class BinarySizeChecker {
 
-  public static void checkBinarySize(String name, long maxSizeByte, String filePath)
+  public static void checkBinarySize(String binaryName, long maxSizeByte, String binaryFilePath)
       throws MobileHarnessException {
     assertWithMessage(
             "The binary size of %s should be less than %s bytes. If you are sure that the new added"
                 + " deps are necessary, please update the number and explain the necessity (what"
                 + " libs are added to the binary, their sizes, why they are necessary) in the"
                 + " change description. file_path=%s",
-            name, maxSizeByte, filePath)
-        .that(new LocalFileUtil().getFileSize(filePath))
+            binaryName, maxSizeByte, binaryFilePath)
+        .that(new LocalFileUtil().getFileSize(binaryFilePath))
         .isLessThan(maxSizeByte);
+  }
+
+  public static void checkBinaryLargeResourceFiles(
+      String binaryName,
+      String binaryFilePath,
+      long maxResourceFileSizeByte,
+      ImmutableSet<String> largeResourcePathAllowlist,
+      String binarySourcePath,
+      String allowlistSourcePath)
+      throws IOException {
+
+    ImmutableMap<String, Long> largeResourcesPathAndSize;
+    try (JarFile jarFile = new JarFile(binaryFilePath)) {
+      largeResourcesPathAndSize =
+          jarFile.stream()
+              .filter(jarEntry -> jarEntry.getSize() > maxResourceFileSizeByte)
+              .sorted(comparingLong(ZipEntry::getSize).reversed())
+              .collect(toImmutableMap(ZipEntry::getName, ZipEntry::getSize));
+    }
+
+    assertWithMessage(
+            "All large resource files in %s must be added to an allowlist. If you believe adding a"
+                + " new large resource file or increasing the size of an existing one is essential,"
+                + " please add the resource path to the allowlist and clearly document the"
+                + " necessity for this inclusion in the cl description.\n\n"
+                + "large_resource_file_allowlist=%s\n"
+                + "resource_file_size_limit=%s(bytes)\n"
+                + "binary_path=%s\n\n"
+                + "Large resource files in %s but not in the allowlist:",
+            binaryName, allowlistSourcePath, maxResourceFileSizeByte, binarySourcePath, binaryName)
+        .that(
+            largeResourcesPathAndSize.entrySet().stream()
+                .filter(e -> !largeResourcePathAllowlist.contains(e.getKey()))
+                .collect(toImmutableMap(Entry::getKey, Entry::getValue)))
+        .isEmpty();
+
+    assertWithMessage(
+            "Please update %s large resource file allowlist to remove items that are not large"
+                + " resources in %s.\n\n"
+                + "large_resource_file_allowlist=%s\n"
+                + "resource_file_size_limit=%s(bytes)\n"
+                + "binary_path=%s\n\n"
+                + "Allowlist items that are not large resource files in %s:",
+            binaryName,
+            binaryName,
+            allowlistSourcePath,
+            maxResourceFileSizeByte,
+            binarySourcePath,
+            binaryName)
+        .that(
+            largeResourcePathAllowlist.stream()
+                .filter(path -> !largeResourcesPathAndSize.containsKey(path))
+                .collect(toImmutableList()))
+        .isEmpty();
   }
 
   private BinarySizeChecker() {}
