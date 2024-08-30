3a0af390fcd0a4ceeb382f5cd4d534f0de271f37
DeviceInfra <no-reply@google.com>
2024-08-30 01:37:08 -0700

    Internal change
    PiperOrigin-RevId: 669238726

--- src/java/com/google/devtools/mobileharness/shared/file/resolver/CacheFileResolver.java
+++ src/java/com/google/devtools/mobileharness/shared/file/resolver/CacheFileResolver.java
@@ -28,6 +28,7 @@ import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.SettableFuture;
 import com.google.devtools.mobileharness.api.model.error.BasicErrorId;
 import com.google.devtools.mobileharness.api.model.error.MobileHarnessException;
+import com.google.devtools.mobileharness.api.model.error.MobileHarnessExceptions;
 import com.google.devtools.mobileharness.shared.util.file.local.LocalFileUtil;
 import com.google.devtools.mobileharness.shared.util.path.PathUtil;
 import java.util.ArrayList;
@@ -94,128 +95,125 @@ public class CacheFileResolver extends AbstractFileResolver {
     CachedResolveSource cachedResolveSource =
         CachedResolveSource.create(resolveSource.path(), resolveSource.parameters());
     SettableFuture<Optional<ResolveResult>> future = SettableFuture.create();
-    logger.atInfo().log("Existing resolved result cache: %s", resolvedResultsCache);
     // ConcurrentHashMap.putIfAbsent guarantees only one future can be really executed for same
     // ResolveSource.
     ListenableFuture<Optional<ResolveResult>> previousFuture =
         resolvedResultsCache.putIfAbsent(cachedResolveSource, future);
-    Object lock = resolvedResultsCache.get(cachedResolveSource);
-    logger.atInfo().log("previousFuture %s, Lock: %s", previousFuture, lock);
-    synchronized (lock) {
-      if (previousFuture == null) {
-        logger.atInfo().log("%s has not been resolved before. Need to resolve.", resolveSource);
-        // If not cached, resolve and set the resolved result to future.
-        try {
-          Optional<ResolveResult> result = super.resolve(resolveSource);
-          future.set(result);
-          return result;
-        } catch (InterruptedException e) {
-          future.setException(
-              new MobileHarnessException(
-                  BasicErrorId.RESOLVE_FILE_TIMEOUT, "Timeout while resolving files", e));
-          throw e;
-        } catch (MobileHarnessException e) {
-          future.setException(e);
-          throw e;
+    if (previousFuture == null) {
+      logger.atInfo().log("%s has not been resolved before. Need to resolve.", resolveSource);
+      // If not cached, resolve and set the resolved result to future.
+      try {
+        Optional<ResolveResult> result = super.resolve(resolveSource);
+        future.set(result);
+        return result;
+      } catch (MobileHarnessException | InterruptedException | Error | RuntimeException e) {
+        future.setException(e);
+        throw e;
+      }
+    } else {
+      logger.atInfo().log("%s has been resolved before. Use the cached value.", resolveSource);
+      // If already cached, use cached future's result.
+      Optional<ResolveResult> optionalResolveResult;
+      try {
+        optionalResolveResult = previousFuture.get(RESOLVE_TIMEOUT_IN_HOUR, HOURS);
+      } catch (ExecutionException e) {
+        if ((e.getCause() instanceof MobileHarnessException
+                && ((MobileHarnessException) e.getCause()).getErrorId()
+                    == BasicErrorId.RESOLVE_FILE_TIMEOUT)
+            || e.getCause() instanceof InterruptedException) {
+          logger.atInfo().log(
+              "Previous resolve process for %s did not succeed because of timeout or interruption."
+                  + " Need to re-resolve.",
+              resolveSource);
+          removeItemIfValueMatch(cachedResolveSource, previousFuture);
+          return internalResolve(resolveSource);
+        } else {
+          return MobileHarnessExceptions.rethrow(e, BasicErrorId.RESOLVE_FILE_GENERIC_ERROR);
         }
-      } else {
-        logger.atInfo().log("%s has been resolved before. Use the cached value.", resolveSource);
-        // If already cached, use cached future's result.
-        Optional<ResolveResult> optionalResolveResult;
-        try {
-          optionalResolveResult = previousFuture.get(RESOLVE_TIMEOUT_IN_HOUR, HOURS);
-        } catch (ExecutionException e) {
-          if (e.getCause() instanceof MobileHarnessException) {
-            MobileHarnessException mobileHarnessException = (MobileHarnessException) e.getCause();
-            if (mobileHarnessException.getErrorId() == BasicErrorId.RESOLVE_FILE_TIMEOUT) {
-              logger.atInfo().log(
-                  "Previous resolve process for %s did not succeed because of timeout. Need to"
-                      + " re-resolve.",
-                  resolveSource);
-              resolvedResultsCache.remove(cachedResolveSource);
-              return internalResolve(resolveSource);
-            } else {
-              throw mobileHarnessException;
-            }
-          } else {
-            throw new MobileHarnessException(
-                BasicErrorId.RESOLVE_FILE_GENERIC_ERROR,
-                String.format("Failed to resolve file %s", resolveSource),
-                e);
-          }
+      } catch (TimeoutException e) {
+        throw new MobileHarnessException(
+            BasicErrorId.RESOLVE_FILE_TIMEOUT,
+            String.format("Timeout while resolving file %s", resolveSource),
+            e);
+      }
 
-        } catch (TimeoutException e) {
-          throw new MobileHarnessException(
-              BasicErrorId.RESOLVE_FILE_TIMEOUT,
-              String.format("Timeout while resolving file %s", resolveSource),
-              e);
+      if (optionalResolveResult.isPresent()) {
+        ResolveResult resolveResult = optionalResolveResult.get();
+        logger.atInfo().log("Previous resolved result: %s", resolveResult);
+        for (String path : resolveResult.paths()) {
+          if (!localFileUtil.isFileOrDirExist(path)) {
+            logger.atInfo().log(
+                "Previous cached resolved file %s for %s doesn't exist any more. Need to"
+                    + " re-resolve.",
+                path, resolveSource);
+            // If any file in the cached result doesn't exist any more, retire cached value
+            // and re-resolve.
+            removeItemIfValueMatch(cachedResolveSource, previousFuture);
+            return internalResolve(resolveSource);
+          }
         }
-
-        if (optionalResolveResult.isPresent()) {
-          ResolveResult resolveResult = optionalResolveResult.get();
-          logger.atInfo().log("Previous resolved result: %s", resolveResult);
-          for (String path : resolveResult.paths()) {
-            if (!localFileUtil.isFileOrDirExist(path)) {
-              logger.atInfo().log(
-                  "Previous cached resolved file %s for %s doesn't exist any more. Need to"
-                      + " re-resolve.",
-                  path, resolveSource);
-              // If any file in the cached result doesn't exist any more, retire cached value
-              // and re-resolve.
-              resolvedResultsCache.remove(cachedResolveSource);
-              return internalResolve(resolveSource);
-            }
-          }
-          if (!resolveResult.resolveSource().equals(resolveSource)) {
-            String cachedRootPath = resolveResult.resolveSource().targetDir();
-            String thisRootPath = resolveSource.targetDir();
-            List<String> thisResolvedPaths = new ArrayList<>();
-            for (String cachedPath : resolveResult.paths()) {
-              String thisPath =
-                  PathUtil.join(thisRootPath, PathUtil.makeRelative(cachedRootPath, cachedPath));
-              if (!localFileUtil.isFileOrDirExist(thisPath)) {
-                try {
-                  localFileUtil.prepareDir(PathUtil.dirname(thisPath));
-                  localFileUtil.copyFileOrDir(cachedPath, thisPath);
-                } catch (MobileHarnessException e) {
-                  // There may be race condition. The cached file is removed after first check.
-                  // So check again.
-                  if (localFileUtil.isFileOrDirExist(cachedPath)
-                      && localFileUtil.getFileOrDirSize(cachedPath)
-                          == localFileUtil.getFileOrDirSize(thisPath)) {
-                    logger.atInfo().withCause(e).log(
-                        "Failed to copy file because of two concurrent copies to same destination."
-                            + " It's acceptable because the former copy succeeds.");
-                  } else {
-                    logger.atInfo().withCause(e).log(
-                        "Previous cached resolved file for %s doesn't exist any more. Need to"
-                            + " re-resolve.",
-                        resolveSource);
-                    // If any file in the cached result doesn't exist any more, retire cached value
-                    // and re-resolve.
-                    resolvedResultsCache.remove(cachedResolveSource);
-                    // Remove already copied file.
-                    for (String path : thisResolvedPaths) {
-                      localFileUtil.removeFileOrDir(path);
-                    }
-                    return internalResolve(resolveSource);
+        if (!resolveResult.resolveSource().equals(resolveSource)) {
+          String cachedRootPath = resolveResult.resolveSource().targetDir();
+          String thisRootPath = resolveSource.targetDir();
+          List<String> thisResolvedPaths = new ArrayList<>();
+          for (String cachedPath : resolveResult.paths()) {
+            String thisPath =
+                PathUtil.join(thisRootPath, PathUtil.makeRelative(cachedRootPath, cachedPath));
+            if (!localFileUtil.isFileOrDirExist(thisPath)) {
+              try {
+                localFileUtil.prepareDir(PathUtil.dirname(thisPath));
+                localFileUtil.copyFileOrDir(cachedPath, thisPath);
+              } catch (MobileHarnessException e) {
+                // There may be race condition. The cached file is removed after first check.
+                // So check again.
+                if (localFileUtil.isFileOrDirExist(cachedPath)
+                    && localFileUtil.getFileOrDirSize(cachedPath)
+                        == localFileUtil.getFileOrDirSize(thisPath)) {
+                  logger.atInfo().withCause(e).log(
+                      "Failed to copy file because of two concurrent copies to same destination."
+                          + " It's acceptable because the former copy succeeds.");
+                } else {
+                  logger.atInfo().withCause(e).log(
+                      "Previous cached resolved file for %s doesn't exist any more. Need to"
+                          + " re-resolve.",
+                      resolveSource);
+                  // If any file in the cached result doesn't exist any more, retire cached value
+                  // and re-resolve.
+                  removeItemIfValueMatch(cachedResolveSource, previousFuture);
+                  // Remove already copied file.
+                  for (String path : thisResolvedPaths) {
+                    localFileUtil.removeFileOrDir(path);
                   }
+                  return internalResolve(resolveSource);
                 }
               }
-              thisResolvedPaths.add(thisPath);
             }
-            ResolveResult result =
-                ResolveResult.create(
-                    ImmutableList.copyOf(thisResolvedPaths),
-                    resolveResult.properties(),
-                    resolveSource);
-            logger.atInfo().log("Resolved result: %s", result);
-            return Optional.of(result);
+            thisResolvedPaths.add(thisPath);
           }
+          ResolveResult result =
+              ResolveResult.create(
+                  ImmutableList.copyOf(thisResolvedPaths),
+                  resolveResult.properties(),
+                  resolveSource);
+          logger.atInfo().log("Resolved result: %s", result);
+          return Optional.of(result);
         }
-        return optionalResolveResult;
       }
+      return optionalResolveResult;
     }
+  }
+
+  private void removeItemIfValueMatch(
+      CachedResolveSource key, ListenableFuture<Optional<ResolveResult>> expectedValue) {
+    resolvedResultsCache.computeIfPresent(
+        key,
+        (k, value) -> {
+          if (value == expectedValue) {
+            return null;
+          } else {
+            return value;
+          }
+        });
   }
 
   @Override
