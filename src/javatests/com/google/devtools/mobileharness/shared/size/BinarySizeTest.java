823cbaa6b54f7733b85a986c59d9b9dab7b0d21a
Shian Hou <shian@google.com>
2024-09-06 07:30:24 -0700

    Internal change
    PiperOrigin-RevId: 671749616

--- src/javatests/com/google/devtools/mobileharness/shared/size/BinarySizeTest.java
+++ src/javatests/com/google/devtools/mobileharness/shared/size/BinarySizeTest.java
@@ -44,7 +44,7 @@ public class BinarySizeTest {
   // Please keep all binary sizes below in the precision "xxx_x50_000L" bytes.
   // ================================================================================
   private static final long MAX_BASE_OLC_SERVER_BINARY_SIZE_BYTE = 29_350_000L;
-  private static final long MAX_ATS_OLC_SERVER_BINARY_SIZE_BYTE = 38_350_000L;
+  private static final long MAX_ATS_OLC_SERVER_BINARY_SIZE_BYTE = 41_450_000L;
   private static final long MAX_ATS_OLC_SERVER_LOCAL_MODE_BINARY_SIZE_BYTE = 40_350_000L;
   private static final long MAX_LAB_SERVER_BINARY_SIZE_BYTE = 37_350_000L;
   private static final long MAX_ATS_CONSOLE_BINARY_SIZE_BYTE = 23_350_000L;
