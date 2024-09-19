a862caa01254e806fd6cb312637e45ed18e3d7c7
Zhenhuang Wang <zeek@google.com>
2024-09-19 00:29:24 -0700

    Internal change
    PiperOrigin-RevId: 676298303

---
+++ src/java/com/google/devtools/mobileharness/platform/android/deviceadmin/DeviceAdminUtil.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.mobileharness.platform.android.deviceadmin;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.devtools.mobileharness.api.model.error.AndroidErrorId;
+import com.google.devtools.mobileharness.api.model.error.MobileHarnessException;
+import com.google.devtools.mobileharness.shared.util.command.Command;
+import com.google.devtools.mobileharness.shared.util.command.CommandException;
+import com.google.devtools.mobileharness.shared.util.command.CommandExecutor;
+import com.google.devtools.mobileharness.shared.util.system.SystemUtil;
+
+/** Utility class for controlling Android device admin. */
+public final class DeviceAdminUtil {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final CommandExecutor commandExecutor;
+  private final String deviceAdminCliPath;
+  private final String kmsKeyName;
+  private final String credPath;
+  private final String adminAppPath;
+  private final SystemUtil systemUtil;
+
+  private static final String ACTION_INSTALL = "INSTALL";
+  private static final String ACTION_ENABLE = "ENABLE";
+  private static final String ACTION_LOCK = "LOCK";
+  private static final String ACTION_UNLOCK = "UNLOCK";
+
+  public DeviceAdminUtil(
+      CommandExecutor commandExecutor,
+      SystemUtil systemUtil,
+      String deviceAdminCliPath,
+      String kmsKeyName,
+      String credPath,
+      String adminAppPath) {
+    this.commandExecutor = commandExecutor;
+    this.systemUtil = systemUtil;
+    this.deviceAdminCliPath = deviceAdminCliPath;
+    this.kmsKeyName = kmsKeyName;
+    this.credPath = credPath;
+    this.adminAppPath = adminAppPath;
+  }
+
+  /**
+   * Installs the device admin app on the device.
+   *
+   * @param deviceId the serial number of the device
+   */
+  public void install(String deviceId) throws MobileHarnessException, InterruptedException {
+    logger.atInfo().log("Installing device admin app on %s", deviceId);
+    try {
+      exec(deviceId, ACTION_INSTALL);
+    } catch (CommandException e) {
+      throw new MobileHarnessException(
+          AndroidErrorId.DEVICE_ADMIN_UTIL_INSTALL_ERROR,
+          "Fail to install the device admin app",
+          e);
+    }
+  }
+
+  /**
+   * Enables the device admin on the device.
+   *
+   * @param deviceId the serial number of the device
+   */
+  public void enable(String deviceId) throws MobileHarnessException, InterruptedException {
+    logger.atInfo().log("Enabling device admin on %s", deviceId);
+    try {
+      exec(deviceId, ACTION_ENABLE);
+    } catch (CommandException e) {
+      throw new MobileHarnessException(
+          AndroidErrorId.DEVICE_ADMIN_UTIL_ENABLE_ERROR, "Fail to enable the device admin app", e);
+    }
+  }
+
+  /**
+   * Locks the device with device admin to enable user restrictions and hide specific apps.
+   *
+   * @param deviceId the serial number of the device
+   */
+  public void lock(String deviceId) throws MobileHarnessException, InterruptedException {
+    logger.atInfo().log("Locking device %s with device admin", deviceId);
+    try {
+      exec(deviceId, ACTION_LOCK);
+    } catch (CommandException e) {
+      throw new MobileHarnessException(
+          AndroidErrorId.DEVICE_ADMIN_UTIL_LOCK_ERROR,
+          "Fail to lock the device with device policies",
+          e);
+    }
+  }
+
+  /**
+   * Unlocks the device to remove user restrictions and allows the device to be reset.
+   *
+   * @param deviceId the serial number of the device
+   */
+  public void unlock(String deviceId) throws MobileHarnessException, InterruptedException {
+    logger.atInfo().log("Unlocking device %s with device policies", deviceId);
+    try {
+      exec(deviceId, ACTION_UNLOCK);
+    } catch (CommandException e) {
+      throw new MobileHarnessException(
+          AndroidErrorId.DEVICE_ADMIN_UTIL_UNLOCK_ERROR,
+          "Fail to unlock the device with device policies",
+          e);
+    }
+  }
+
+  /**
+   * Sets up and locks the device with device admin.
+   *
+   * <p>This action will install the device admin app and lock the device.
+   *
+   * @param deviceId the serial number of the device
+   */
+  public void setupAndLock(String deviceId) throws MobileHarnessException, InterruptedException {
+    logger.atInfo().log("Setup and lock device %s", deviceId);
+    install(deviceId);
+    enable(deviceId);
+    lock(deviceId);
+  }
+
+  private void exec(String deviceId, String action)
+      throws MobileHarnessException, InterruptedException {
+    Command command =
+        Command.of(
+            systemUtil.getJavaBin(),
+            "-jar",
+            deviceAdminCliPath,
+            "--action=" + action,
+            "--serial=" + deviceId,
+            "--kms_key_name=" + kmsKeyName,
+            "--credentials_path=" + credPath);
+
+    if (action.equals(ACTION_INSTALL)) {
+      command = command.argsAppended("--admin_app_path=" + adminAppPath);
+    }
+    logger.atInfo().log("Device admin util is running command %s on %s", command, deviceId);
+    commandExecutor.run(command);
+  }
+}
---
+++ src/javatests/com/google/devtools/mobileharness/platform/android/deviceadmin/DeviceAdminUtilTest.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.mobileharness.platform.android.deviceadmin;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.devtools.mobileharness.api.model.error.AndroidErrorId;
+import com.google.devtools.mobileharness.api.model.error.MobileHarnessException;
+import com.google.devtools.mobileharness.shared.util.command.Command;
+import com.google.devtools.mobileharness.shared.util.command.CommandException;
+import com.google.devtools.mobileharness.shared.util.command.CommandExecutor;
+import com.google.devtools.mobileharness.shared.util.system.SystemUtil;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+@RunWith(JUnit4.class)
+public final class DeviceAdminUtilTest {
+  private static final String DEVICE_ID = "device_id";
+  private static final String DEVICE_ADMIN_CLI_PATH = "/path/to/device_admin_cli.jar";
+  private static final String KMS_KEY_NAME =
+      "projects/foo-project/locations/global/keyRings/foo-keyRing/cryptoKeys/foo-key/cryptoKeyVersions/1";
+  private static final String CRED_PATH = "/path/to/cred";
+  private static final String ADMIN_APP_PATH = "/path/to/admin_app.apk";
+  private static final String JAVA_BIN = "/path/to/java";
+
+  @Rule public final MockitoRule mockito = MockitoJUnit.rule();
+  @Mock private CommandExecutor commandExecutor;
+  @Mock private SystemUtil systemUtil;
+
+  DeviceAdminUtil deviceAdminUtil;
+
+  @Before
+  public void setUp() {
+    this.deviceAdminUtil =
+        new DeviceAdminUtil(
+            commandExecutor,
+            systemUtil,
+            DEVICE_ADMIN_CLI_PATH,
+            KMS_KEY_NAME,
+            CRED_PATH,
+            ADMIN_APP_PATH);
+    when(systemUtil.getJavaBin()).thenReturn(JAVA_BIN);
+  }
+
+  @Test
+  public void install_success_commandExecuted() throws Exception {
+    deviceAdminUtil.install(DEVICE_ID);
+
+    verify(commandExecutor)
+        .run(
+            Command.of(
+                JAVA_BIN,
+                "-jar",
+                DEVICE_ADMIN_CLI_PATH,
+                "--action=INSTALL",
+                "--serial=" + DEVICE_ID,
+                "--kms_key_name=" + KMS_KEY_NAME,
+                "--credentials_path=" + CRED_PATH,
+                "--admin_app_path=" + ADMIN_APP_PATH));
+  }
+
+  @Test
+  public void install_commandFail_throwException() throws Exception {
+    when(commandExecutor.run(any())).thenThrow(CommandException.class);
+
+    MobileHarnessException e =
+        assertThrows(MobileHarnessException.class, () -> deviceAdminUtil.install(DEVICE_ID));
+    assertThat(e.getErrorId()).isEqualTo(AndroidErrorId.DEVICE_ADMIN_UTIL_INSTALL_ERROR);
+  }
+
+  @Test
+  public void lock_success_commandExecuted() throws Exception {
+    deviceAdminUtil.lock(DEVICE_ID);
+
+    verify(commandExecutor)
+        .run(
+            Command.of(
+                JAVA_BIN,
+                "-jar",
+                DEVICE_ADMIN_CLI_PATH,
+                "--action=LOCK",
+                "--serial=" + DEVICE_ID,
+                "--kms_key_name=" + KMS_KEY_NAME,
+                "--credentials_path=" + CRED_PATH));
+  }
+
+  @Test
+  public void lock_commandFail_throwException() throws Exception {
+    when(commandExecutor.run(any())).thenThrow(CommandException.class);
+
+    MobileHarnessException e =
+        assertThrows(MobileHarnessException.class, () -> deviceAdminUtil.lock(DEVICE_ID));
+    assertThat(e.getErrorId()).isEqualTo(AndroidErrorId.DEVICE_ADMIN_UTIL_LOCK_ERROR);
+  }
+
+  @Test
+  public void unlock_success_commandExecuted() throws Exception {
+    deviceAdminUtil.unlock(DEVICE_ID);
+
+    verify(commandExecutor)
+        .run(
+            Command.of(
+                JAVA_BIN,
+                "-jar",
+                DEVICE_ADMIN_CLI_PATH,
+                "--action=UNLOCK",
+                "--serial=" + DEVICE_ID,
+                "--kms_key_name=" + KMS_KEY_NAME,
+                "--credentials_path=" + CRED_PATH));
+  }
+
+  @Test
+  public void unlock_commandFail_throwException() throws Exception {
+    when(commandExecutor.run(any())).thenThrow(CommandException.class);
+
+    MobileHarnessException e =
+        assertThrows(MobileHarnessException.class, () -> deviceAdminUtil.unlock(DEVICE_ID));
+    assertThat(e.getErrorId()).isEqualTo(AndroidErrorId.DEVICE_ADMIN_UTIL_UNLOCK_ERROR);
+  }
+
+  @Test
+  public void enable_success_commandExecuted() throws Exception {
+    deviceAdminUtil.enable(DEVICE_ID);
+
+    verify(commandExecutor)
+        .run(
+            Command.of(
+                JAVA_BIN,
+                "-jar",
+                DEVICE_ADMIN_CLI_PATH,
+                "--action=ENABLE",
+                "--serial=" + DEVICE_ID,
+                "--kms_key_name=" + KMS_KEY_NAME,
+                "--credentials_path=" + CRED_PATH));
+  }
+
+  @Test
+  public void enable_commandFail_throwException() throws Exception {
+    when(commandExecutor.run(any())).thenThrow(CommandException.class);
+
+    MobileHarnessException e =
+        assertThrows(MobileHarnessException.class, () -> deviceAdminUtil.enable(DEVICE_ID));
+    assertThat(e.getErrorId()).isEqualTo(AndroidErrorId.DEVICE_ADMIN_UTIL_ENABLE_ERROR);
+  }
+
+  @Test
+  public void setupAndLock_success_commandExecuted() throws Exception {
+    deviceAdminUtil.setupAndLock(DEVICE_ID);
+
+    verify(commandExecutor)
+        .run(
+            Command.of(
+                JAVA_BIN,
+                "-jar",
+                DEVICE_ADMIN_CLI_PATH,
+                "--action=INSTALL",
+                "--serial=" + DEVICE_ID,
+                "--kms_key_name=" + KMS_KEY_NAME,
+                "--credentials_path=" + CRED_PATH,
+                "--admin_app_path=" + ADMIN_APP_PATH));
+
+    verify(commandExecutor)
+        .run(
+            Command.of(
+                JAVA_BIN,
+                "-jar",
+                DEVICE_ADMIN_CLI_PATH,
+                "--action=ENABLE",
+                "--serial=" + DEVICE_ID,
+                "--kms_key_name=" + KMS_KEY_NAME,
+                "--credentials_path=" + CRED_PATH));
+
+    verify(commandExecutor)
+        .run(
+            Command.of(
+                JAVA_BIN,
+                "-jar",
+                DEVICE_ADMIN_CLI_PATH,
+                "--action=LOCK",
+                "--serial=" + DEVICE_ID,
+                "--kms_key_name=" + KMS_KEY_NAME,
+                "--credentials_path=" + CRED_PATH));
+  }
+}
