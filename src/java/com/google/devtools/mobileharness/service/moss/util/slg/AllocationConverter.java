81db5bcdcb505dc410e58f42c6f0c09a1dffdb66
Shian Hou <shian@google.com>
2024-09-25 04:53:04 -0700

    Internal change
    PiperOrigin-RevId: 678649308

--- src/java/com/google/devtools/mobileharness/service/moss/util/slg/AllocationConverter.java
+++ src/java/com/google/devtools/mobileharness/service/moss/util/slg/AllocationConverter.java
@@ -16,14 +16,16 @@
 
 package com.google.devtools.mobileharness.service.moss.util.slg;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Multimap;
+import com.google.devtools.mobileharness.api.model.allocation.Allocation;
+import com.google.devtools.mobileharness.api.model.lab.DeviceLocator;
 import com.google.devtools.mobileharness.service.moss.proto.Slg.AllocationProto;
 import com.google.devtools.mobileharness.service.moss.proto.Slg.AllocationProto.DimensionEntry;
 import com.google.devtools.mobileharness.service.moss.proto.Slg.AllocationProto.DimensionMultiMap;
-import com.google.wireless.qa.mobileharness.shared.model.allocation.Allocation;
 import com.google.wireless.qa.mobileharness.shared.model.allocation.AllocationInternalFactory;
-import com.google.wireless.qa.mobileharness.shared.model.lab.DeviceLocator;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -31,22 +33,50 @@ import java.util.stream.Collectors;
  * Utility class to convert a {@link Allocation} to a {@link AllocationProto} in forward and
  * backward.
  */
-final class AllocationConverter {
+public final class AllocationConverter {
 
   private AllocationConverter() {}
 
   /** Gets a {@link Allocation} by the given {@link AllocationProto}. */
-  static Allocation fromProto(AllocationProto allocationProto) {
+  public static Allocation toNewAllocation(AllocationProto allocationProto) {
+    return fromProto(allocationProto).toNewAllocation();
+  }
+
+  /**
+   * Gets a {@linkplain com.google.wireless.qa.mobileharness.shared.model.allocation.Allocation
+   * Allocation} by the given {@link AllocationProto}.
+   */
+  public static com.google.wireless.qa.mobileharness.shared.model.allocation.Allocation fromProto(
+      AllocationProto allocationProto) {
     return AllocationInternalFactory.createAllocation(allocationProto);
   }
 
   /** Gets a {@link AllocationProto} by the given {@link Allocation}. */
-  static AllocationProto toProto(Allocation allocation) {
+  public static AllocationProto toProto(Allocation allocation) {
+    return AllocationProto.newBuilder()
+        .setTestLocator(allocation.getTest().toProto())
+        .addAllDeviceLocator(
+            allocation.getAllDevices().stream()
+                .map(DeviceLocator::toProto)
+                .collect(toImmutableList()))
+        .build();
+  }
+
+  /**
+   * Gets a {@link AllocationProto} by the given {@linkplain
+   * com.google.wireless.qa.mobileharness.shared.model.allocation.Allocation Allocation}.
+   */
+  public static AllocationProto toProto(
+      com.google.wireless.qa.mobileharness.shared.model.allocation.Allocation allocation) {
     AllocationProto.Builder allocationProto =
         AllocationProto.newBuilder()
             .setTestLocator(allocation.getTest().toNewTestLocator().toProto());
-    ImmutableList<DeviceLocator> deviceLocators = allocation.getAllDeviceLocators();
-    Map<DeviceLocator, Multimap<String, String>> allDevices = allocation.getAllDevices();
+    ImmutableList<com.google.wireless.qa.mobileharness.shared.model.lab.DeviceLocator>
+        deviceLocators = allocation.getAllDeviceLocators();
+    Map<
+            com.google.wireless.qa.mobileharness.shared.model.lab.DeviceLocator,
+            Multimap<String, String>>
+        allDevices = allocation.getAllDevices();
     deviceLocators.forEach(
         deviceLocator -> {
           Multimap<String, String> dimensions = allDevices.get(deviceLocator);
