83eb982ceec0dfc28d6fd1097793d4da9d8e7317
DeviceInfra <no-reply@google.com>
2024-11-12 02:48:15 -0800

    Internal change
    PiperOrigin-RevId: 695657272

--- src/java/com/google/devtools/mobileharness/shared/version/Version.java
+++ src/java/com/google/devtools/mobileharness/shared/version/Version.java
@@ -28,7 +28,7 @@ public final class Version implements Comparable<Version> {
 
   public static final Version MIN_CLIENT_VERSION = new Version(4, 29, 0);
 
-  public static final Version LAB_VERSION = new Version(4, 297, 0);
+  public static final Version LAB_VERSION = new Version(4, 297, 1);
 
   public static final Version MIN_LAB_VERSION = new Version(4, 25, 0);
 
