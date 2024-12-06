9c455bd81ba2d26efd9fd565e7a9ea1aaa7f8011
DeviceInfra <no-reply@google.com>
2024-12-05 23:55:20 -0800

    Internal change
    PiperOrigin-RevId: 703388317

--- src/java/com/google/wireless/qa/mobileharness/shared/api/driver/XtsTradefedTest.java
+++ src/java/com/google/wireless/qa/mobileharness/shared/api/driver/XtsTradefedTest.java
@@ -135,7 +135,7 @@ public class XtsTradefedTest extends BaseDriver
           "--compatibility:exclude-filter");
 
   private static final ImmutableSet<String> DYNAMIC_JOB_TEST_DEPENDENCIES =
-      ImmutableSet.of("CtsPreconditions", "CtsDeviceInfo", "CtsUsbTests");
+      ImmutableSet.of("CtsPreconditions", "CtsDeviceInfo", "CtsUsbTests", "CtsUsbSerialTestApp");
 
   // Will remove these cases from MCTS.
   private static final ImmutableSet<String> STATIC_JOB_TEST_DEPENDENCIES =
