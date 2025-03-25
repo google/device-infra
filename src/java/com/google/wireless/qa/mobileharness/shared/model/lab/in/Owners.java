a2018b57fee6a01fe0a1a3c83ab3c911c303c235
Shian Hou <shian@google.com>
2025-03-24 18:56:31 -0700

    Internal change
    PiperOrigin-RevId: 740148922

--- src/java/com/google/wireless/qa/mobileharness/shared/model/lab/in/Owners.java
+++ src/java/com/google/wireless/qa/mobileharness/shared/model/lab/in/Owners.java
@@ -21,59 +21,71 @@ import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import javax.annotation.concurrent.GuardedBy;
 
 /** Owners of a device. */
 public class Owners {
-  /** Device owners. Or empty if the device is public. */
+
+  /**
+   * Device owners who can config the device and run tests on the device.
+   *
+   * <p>Empty if the device is public.
+   */
+  @GuardedBy("itself")
   private final Set<String> owners = new HashSet<>();
 
   /** Add a device owner. */
   @CanIgnoreReturnValue
-  public synchronized Owners add(String owner) {
-    owners.add(owner);
+  public Owners add(String owner) {
+    synchronized (owners) {
+      owners.add(owner);
+    }
     return this;
   }
 
   /** Adds device owners. */
   @CanIgnoreReturnValue
-  public synchronized Owners addAll(Collection<String> owners) {
-    this.owners.addAll(owners);
-    return this;
-  }
-
-  /** Adds device owners. */
-  @CanIgnoreReturnValue
-  public synchronized Owners addAll(String[] owners) {
-    for (String owner : owners) {
-      this.owners.add(owner);
+  public Owners addAll(Collection<String> owners) {
+    synchronized (this.owners) {
+      this.owners.addAll(owners);
     }
     return this;
   }
 
   /** Replace the current device owners with the given ones. */
   @CanIgnoreReturnValue
-  public synchronized Owners setAll(Collection<String> owners) {
-    this.owners.clear();
-    return addAll(owners);
+  public Owners setAll(Collection<String> owners) {
+    synchronized (this.owners) {
+      this.owners.clear();
+      return addAll(owners);
+    }
   }
 
   /** Returns whether the device owners is empty, which means the device is public. */
   public boolean isPublic() {
-    return owners.isEmpty();
+    synchronized (owners) {
+      return owners.isEmpty();
+    }
   }
 
   /** Returns the number of device owners. */
   public int size() {
-    return owners.size();
+    synchronized (owners) {
+      return owners.size();
+    }
   }
 
   /** Gets all the device owners, or empty if the device is public. */
-  public synchronized Set<String> getAll() {
-    return ImmutableSet.copyOf(owners);
+  public Set<String> getAll() {
+    synchronized (owners) {
+      return ImmutableSet.copyOf(owners);
+    }
   }
 
-  /** Checks whether the user is in the owner list of the device. */
-  public synchronized boolean support(String user) {
-    return owners.isEmpty() || owners.contains(user);
+  /** Checks whether the user is in the owner list of the device or the device is public. */
+  public boolean support(String user) {
+    synchronized (owners) {
+      return isPublic() || owners.contains(user);
+    }
   }
 }
