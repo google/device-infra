437cdd0d1cd05e577193c7f5cd315f5e3a0e177f
Shian Hou <shian@google.com>
2025-05-24 02:14:09 -0700

    Internal change
    PiperOrigin-RevId: 762765429

---
+++ src/java/com/google/devtools/mobileharness/api/testrunner/step/android/DeviceInitializationStep.java
@@ -0,0 +1,319 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.mobileharness.api.testrunner.step.android;
+
+import static java.lang.Math.max;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.flogger.FluentLogger;
+import com.google.devtools.mobileharness.api.model.error.AndroidErrorId;
+import com.google.devtools.mobileharness.api.model.error.MobileHarnessException;
+import com.google.devtools.mobileharness.api.testrunner.device.cache.DeviceCache;
+import com.google.devtools.mobileharness.platform.android.app.devicedaemon.DeviceDaemonHelper;
+import com.google.devtools.mobileharness.platform.android.device.AndroidDeviceHelper;
+import com.google.devtools.mobileharness.platform.android.devicestate.AndroidDeviceInitializer;
+import com.google.devtools.mobileharness.platform.android.lightning.shared.SharedLogUtil;
+import com.google.devtools.mobileharness.shared.util.base.StrUtil;
+import com.google.devtools.mobileharness.shared.util.time.Sleeper;
+import com.google.wireless.qa.mobileharness.shared.api.annotation.ParamAnnotation;
+import com.google.wireless.qa.mobileharness.shared.api.device.AndroidDevice;
+import com.google.wireless.qa.mobileharness.shared.api.device.Device;
+import com.google.wireless.qa.mobileharness.shared.api.spec.AndroidRealDeviceSpec;
+import com.google.wireless.qa.mobileharness.shared.model.job.TestInfo;
+import com.google.wireless.qa.mobileharness.shared.model.job.out.Log;
+import java.time.Duration;
+import java.util.Optional;
+import java.util.logging.Level;
+import javax.annotation.Nullable;
+
+/** Utility methods used for device initialization in after-flash and after-factory-reset. */
+public class DeviceInitializationStep {
+
+  @ParamAnnotation(
+      required = false,
+      help =
+          "If set to 'true', setup wizard will be skipped after flashing or factory reset, default"
+              + " to be 'false'. It works on Google Experience Device with userdebug build only."
+              + " Currently this param needs to be set in the job params level.")
+  public static final String PARAM_SKIP_SETUP_WIZARD = "skip_setup_wizard";
+
+  @ParamAnnotation(
+      required = false,
+      help = "If true, disable dm-verity after flashing or factory reset device.")
+  public static final String PARAM_DISABLE_VERITY = "disable_verity";
+
+  @ParamAnnotation(
+      required = false,
+      help = "Timeout to wait for device online after flashing or factory reset.")
+  public static final String PARAM_DEVICE_READY_TIMEOUT_SEC = "device_ready_timeout_sec";
+
+  @ParamAnnotation(
+      required = false,
+      help = "The number of attempts for waiting for device initialization. Default is 3.")
+  public static final String PARAM_WAIT_FOR_DEVICE_INIT_ATTEMPTS = "wait_for_device_init_attempts";
+
+  // Timeout to wait for device online after flashing or factory reset.
+  @VisibleForTesting static final Duration DEFAULT_DEVICE_READY_TIMEOUT = Duration.ofMinutes(15);
+
+  /** Minimum device ready time for {@link #PARAM_DEVICE_READY_TIMEOUT_SEC} */
+  public static final Duration MIN_DEVICE_READY_TIMEOUT = Duration.ofMinutes(1);
+
+  /** Maximum device ready time for {@link #PARAM_DEVICE_READY_TIMEOUT_SEC} */
+  public static final Duration MAX_DEVICE_READY_TIMEOUT = Duration.ofMinutes(30);
+
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private static final Duration CACHE_TIME_INIT = Duration.ofMinutes(25);
+
+  private static final Duration DEVICE_INIT_EXTRA_CACHE_DEVICE_TIME = Duration.ofMinutes(5);
+
+  private final AndroidDeviceInitializer androidDeviceInitializer;
+
+  private final DeviceDaemonHelper deviceDaemonHelper;
+
+  private final Sleeper sleeper;
+
+  private final AndroidDeviceHelper androidDeviceHelper;
+
+  public DeviceInitializationStep() {
+    this(
+        new AndroidDeviceInitializer(),
+        new DeviceDaemonHelper(),
+        Sleeper.defaultSleeper(),
+        new AndroidDeviceHelper());
+  }
+
+  @VisibleForTesting
+  DeviceInitializationStep(
+      AndroidDeviceInitializer androidDeviceInitializer,
+      DeviceDaemonHelper deviceDaemonHelper,
+      Sleeper sleeper,
+      AndroidDeviceHelper androidDeviceHelper) {
+    this.androidDeviceInitializer = androidDeviceInitializer;
+    this.deviceDaemonHelper = deviceDaemonHelper;
+    this.sleeper = sleeper;
+    this.androidDeviceHelper = androidDeviceHelper;
+  }
+
+  /**
+   * Initialize device after flash or factory reset.
+   *
+   * <p>It'll update device property for sdk version by default.
+   */
+  public void initializeDevice(Device device, TestInfo testInfo)
+      throws MobileHarnessException, InterruptedException {
+    initializeDevice(
+        device,
+        testInfo,
+        /* skipUpdateDevicePropertiesAndDimensions= */ false,
+        /* skipCacheDevice= */ false);
+  }
+
+  /**
+   * Initialize device after flash or factory reset without additional information from TestInfo.
+   *
+   * <p>It'll update device property for sdk version by default.
+   */
+  public void initializeDevice(Device device) throws MobileHarnessException, InterruptedException {
+    initializeDevice(
+        device,
+        /* testInfo= */ null,
+        /* skipUpdateDevicePropertiesAndDimensions= */ false,
+        /* skipCacheDevice= */ false);
+  }
+
+  /**
+   * Initialize device after flash or factory reset.
+   *
+   * @param skipUpdateDevicePropertiesAndDimensions {@code true} if want to skip updating device
+   *     properties and dimensions.
+   */
+  public void initializeDevice(
+      Device device, TestInfo testInfo, boolean skipUpdateDevicePropertiesAndDimensions)
+      throws MobileHarnessException, InterruptedException {
+    initializeDevice(
+        device, testInfo, skipUpdateDevicePropertiesAndDimensions, /* skipCacheDevice= */ false);
+  }
+
+  /**
+   * Initialize device after flash or factory reset.
+   *
+   * @param skipCacheDevice {@code true} if want to skip caching device during initialization. If
+   *     skip cache device, you need to explicitly cache the device before the initialization starts
+   *     and invalidate the cache after it ends.
+   */
+  public void initializeDevice(
+      Device device,
+      @Nullable TestInfo testInfo,
+      boolean skipUpdateDevicePropertiesAndDimensions,
+      boolean skipCacheDevice)
+      throws MobileHarnessException, InterruptedException {
+    waitForDeviceInitializedWithRetry(device, testInfo, skipCacheDevice);
+    if (!skipUpdateDevicePropertiesAndDimensions) {
+      updateDevicePropertiesAndDimensions(device);
+    }
+    installAndStartDaemon(device, testInfo);
+  }
+
+  private void waitForDeviceInitializedWithRetry(
+      Device device, @Nullable TestInfo testInfo, boolean skipCacheDevice)
+      throws MobileHarnessException, InterruptedException {
+    Log log = testInfo != null ? testInfo.log() : null;
+    String deviceId = device.getDeviceId();
+    SharedLogUtil.logMsg(logger, log, "Starting device initialization for %s", deviceId);
+
+    Duration sleepLength = Duration.ofSeconds(30);
+    int deviceInitAttempts =
+        testInfo == null
+            ? 1
+            : max(
+                1,
+                testInfo
+                    .jobInfo()
+                    .params()
+                    .getInt(PARAM_WAIT_FOR_DEVICE_INIT_ATTEMPTS, /* defaultValue= */ 3));
+    Optional<Duration> deviceReadyTimeout = getDeviceReadyTimeout(testInfo);
+    if (!skipCacheDevice) {
+      cacheDevice(
+          device,
+          CACHE_TIME_INIT
+              .plus(deviceReadyTimeout.orElse(DEVICE_INIT_EXTRA_CACHE_DEVICE_TIME))
+              .plus(sleepLength)
+              .multipliedBy(deviceInitAttempts));
+    }
+    try {
+      for (int i = 0; i < deviceInitAttempts; i++) {
+        SharedLogUtil.logMsg(
+            logger,
+            log,
+            "Waiting for %s init with timeout %s at attempt %d/%d",
+            deviceId,
+            deviceReadyTimeout,
+            i + 1,
+            deviceInitAttempts);
+        try {
+          androidDeviceInitializer.initialize(
+              deviceId,
+              testInfo != null && testInfo.jobInfo().params().isTrue(PARAM_SKIP_SETUP_WIZARD),
+              testInfo != null && testInfo.jobInfo().params().isTrue(PARAM_DISABLE_VERITY),
+              deviceReadyTimeout);
+          break;
+        } catch (MobileHarnessException e) {
+          if (i + 1 == deviceInitAttempts) {
+            throw new MobileHarnessException(
+                AndroidErrorId.ANDROID_DEVICE_INIT_STEP_INIT_DEVICE_ERROR,
+                String.format(
+                    "Failed all initialization attempts (%d attempts). Original failure was set"
+                        + " as cause of this exception.",
+                    deviceInitAttempts),
+                e);
+          }
+
+          if (testInfo != null) {
+            Duration remainingTime = testInfo.timer().remainingTimeJava();
+            if (remainingTime.compareTo(sleepLength) < 0) {
+              throw new MobileHarnessException(
+                  AndroidErrorId.ANDROID_DEVICE_INIT_STEP_INIT_DEVICE_NO_ENOUGH_TIME_LEFT,
+                  String.format(
+                      "Failed device initialization and no time left to retry. Remaining time %dms,"
+                          + " at least %dms needed.",
+                      remainingTime.toMillis(), sleepLength.toMillis()),
+                  e);
+            }
+          }
+
+          SharedLogUtil.logMsg(
+              logger,
+              Level.WARNING,
+              log,
+              /* cause= */ null,
+              "Failed device initialization attempt %d/%d due to %s, retrying",
+              i + 1,
+              deviceInitAttempts,
+              e.getMessage());
+          sleeper.sleep(sleepLength);
+        }
+      }
+    } finally {
+      if (!skipCacheDevice) {
+        invalidateCacheDevice(device);
+      }
+    }
+  }
+
+  private void updateDevicePropertiesAndDimensions(Device device)
+      throws MobileHarnessException, InterruptedException {
+    if (device instanceof AndroidDevice) {
+      AndroidDevice androidDevice = ((AndroidDevice) device);
+      logger.atInfo().log(
+          "Updating Android device %s properties and dimensions if needed.", device.getDeviceId());
+      androidDeviceHelper.updateAndroidPropertyDimensions(androidDevice);
+    }
+  }
+
+  private void installAndStartDaemon(Device device, @Nullable TestInfo testInfo)
+      throws MobileHarnessException, InterruptedException {
+    try {
+      if (testInfo == null) {
+        deviceDaemonHelper.installAndStartDaemon(device, /* log= */ null);
+      } else if (!testInfo
+          .jobInfo()
+          .params()
+          .getBool(AndroidRealDeviceSpec.PARAM_KILL_DAEMON_ON_TEST, false /* default value */)) {
+        deviceDaemonHelper.installAndStartDaemon(device, testInfo.log());
+      }
+    } catch (MobileHarnessException e) {
+      throw new MobileHarnessException(
+          AndroidErrorId.ANDROID_DEVICE_INIT_STEP_START_DEVICE_DAEMON_ERROR,
+          String.format("Failed to install and start MH Daemon on device %s", device.getDeviceId()),
+          e);
+    }
+  }
+
+  private void cacheDevice(Device device, Duration expireTime) {
+    DeviceCache.getInstance()
+        .cache(device.getDeviceId(), device.getClass().getSimpleName(), expireTime);
+  }
+
+  private void invalidateCacheDevice(Device device) {
+    DeviceCache.getInstance().invalidateCache(device.getDeviceId());
+  }
+
+  private Optional<Duration> getDeviceReadyTimeout(@Nullable TestInfo testInfo)
+      throws MobileHarnessException {
+    if (testInfo == null) {
+      return Optional.of(DEFAULT_DEVICE_READY_TIMEOUT);
+    }
+
+    if (StrUtil.isEmptyOrWhitespace(
+        testInfo.jobInfo().params().get(PARAM_DEVICE_READY_TIMEOUT_SEC))) {
+      return Optional.empty();
+    }
+
+    Duration deviceReadyTimeout = null;
+    try {
+      // No need to check if deviceReadyTimeout is in correct range. It has been verified in
+      // validator.
+      deviceReadyTimeout =
+          Duration.ofSeconds(testInfo.jobInfo().params().getLong(PARAM_DEVICE_READY_TIMEOUT_SEC));
+    } catch (MobileHarnessException e) {
+      throw new MobileHarnessException(
+          AndroidErrorId.ANDROID_DEVICE_INIT_STEP_GET_JOB_PARAM_ERROR, e.getMessage(), e);
+    }
+    return Optional.of(deviceReadyTimeout);
+  }
+}
---
+++ src/java/com/google/devtools/mobileharness/platform/android/devicestate/AndroidDeviceInitializer.java
@@ -0,0 +1,225 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.mobileharness.platform.android.devicestate;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.flogger.FluentLogger;
+import com.google.devtools.mobileharness.api.model.error.MobileHarnessException;
+import com.google.devtools.mobileharness.platform.android.packagemanager.AndroidPackageManagerUtil;
+import com.google.devtools.mobileharness.platform.android.sdktool.adb.DeviceConnectionState;
+import com.google.devtools.mobileharness.platform.android.systemsetting.AndroidSystemSettingUtil;
+import com.google.devtools.mobileharness.platform.android.systemsetting.PostSetDmVerityDeviceOp;
+import com.google.devtools.mobileharness.platform.android.systemsetting.PostSettingDeviceOp;
+import com.google.devtools.mobileharness.platform.android.systemspec.AndroidSystemSpecUtil;
+import com.google.devtools.mobileharness.platform.android.systemstate.AndroidSystemStateUtil;
+import com.google.devtools.mobileharness.shared.util.time.Sleeper;
+import com.google.wireless.qa.mobileharness.shared.util.DeviceUtil;
+import java.time.Duration;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * The device initialize util for setting up a new device, since the device will reboot several
+ * times, need cache the device before calling if using it in lab server.
+ */
+public class AndroidDeviceInitializer {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  /** Device feature indicating it is an AndroidThings (IoT) device. */
+  @VisibleForTesting
+  static final String FEATURE_EMBEDDED = "feature:android.hardware.type.embedded";
+
+  /** Device feature indicating it is an Android Automotive device. */
+  @VisibleForTesting
+  static final String FEATURE_AUTOMOTIVE = "feature:android.hardware.type.automotive";
+
+  /** Default timeout of waiting device to be ready. */
+  @VisibleForTesting static final Duration DEFAULT_DEVICE_READY_TIMEOUT = Duration.ofMinutes(5);
+
+  private final AndroidSystemSettingUtil systemSettingUtil;
+  private final AndroidSystemStateUtil systemStateUtil;
+  private final AndroidSystemSpecUtil systemSpecUtil;
+  private final AndroidPackageManagerUtil packageManagerUtil;
+  private final Sleeper sleeper;
+
+  public AndroidDeviceInitializer() {
+    this(
+        Sleeper.defaultSleeper(),
+        new AndroidSystemSettingUtil(),
+        new AndroidSystemStateUtil(),
+        new AndroidSystemSpecUtil(),
+        new AndroidPackageManagerUtil());
+  }
+
+  @VisibleForTesting
+  AndroidDeviceInitializer(
+      Sleeper sleeper,
+      AndroidSystemSettingUtil systemSettingUtil,
+      AndroidSystemStateUtil systemStateUtil,
+      AndroidSystemSpecUtil systemSpecUtil,
+      AndroidPackageManagerUtil packageManagerUtil) {
+    this.sleeper = sleeper;
+    this.systemSettingUtil = systemSettingUtil;
+    this.systemStateUtil = systemStateUtil;
+    this.systemSpecUtil = systemSpecUtil;
+    this.packageManagerUtil = packageManagerUtil;
+  }
+
+  /**
+   * Initializes the device.
+   *
+   * <p>Timeout of waiting for device to be ready is defaulted to {@link
+   * #DEFAULT_DEVICE_READY_TIMEOUT}
+   *
+   * @param deviceId the device serial number
+   * @param skipWizard whether to skip wizard
+   */
+  public void initialize(String deviceId, boolean skipWizard, boolean disableVerity)
+      throws MobileHarnessException, InterruptedException {
+    initialize(deviceId, skipWizard, disableVerity, Optional.of(DEFAULT_DEVICE_READY_TIMEOUT));
+  }
+
+  /**
+   * Initializes the device:
+   *
+   * <ol>
+   *   <li>become root.
+   *   <li>skip wizard and dm verity if necessary.
+   *   <li>allow mock location.
+   *   <li>keep awake.
+   *   <li>disable screen lock.
+   *   <li>disable package verifier.
+   *   <li>enable unknown resource.
+   * </ol>
+   *
+   * <p>Cache the device out the function if you are using it in lab server to avoid device manager
+   * kill the process.
+   *
+   * @param deviceId the device serial number
+   * @param skipWizard whether to skip wizard
+   * @param checkReadyTimeout max wait time for checking the readiness of device
+   */
+  public void initialize(
+      String deviceId,
+      boolean skipWizard,
+      boolean disableVerity,
+      Optional<Duration> checkReadyTimeout)
+      throws MobileHarnessException, InterruptedException {
+    Duration deviceReadyTimeout = checkReadyTimeout.orElse(DEFAULT_DEVICE_READY_TIMEOUT);
+    systemStateUtil.waitForState(deviceId, DeviceConnectionState.DEVICE, Duration.ofMinutes(5));
+    systemStateUtil.waitUntilReady(deviceId, deviceReadyTimeout);
+    var unused = systemStateUtil.becomeRoot(deviceId);
+    systemStateUtil.waitUntilReady(deviceId, Duration.ofMinutes(3));
+
+    // If Test Harness Mode is enabled, we can skip some settings as it has been done for us.
+    boolean isTestHarnessModeEnabled = systemSettingUtil.isTestHarnessModeEnabled(deviceId);
+    logger.atInfo().log(
+        "Test Harness Mode state on device %s: %s", deviceId, isTestHarnessModeEnabled);
+
+    boolean needsRebootAfterSettingChanges = false;
+    if (skipWizard && !isTestHarnessModeEnabled) {
+      logger.atInfo().log("Skipping setup wizard");
+      if (systemSettingUtil.disableSetupWizard(deviceId) == PostSettingDeviceOp.REBOOT) {
+        needsRebootAfterSettingChanges = true;
+      }
+    }
+    if (disableVerity) {
+      if (systemSettingUtil.getDeviceSdkVersion(deviceId) >= 22
+          && systemSettingUtil.setDmVerityChecking(deviceId, false)
+              == PostSetDmVerityDeviceOp.REBOOT) {
+        logger.atInfo().log("Disabled dm-verity.");
+        needsRebootAfterSettingChanges = true;
+      }
+    }
+    if (needsRebootAfterSettingChanges) {
+      systemStateUtil.reboot(deviceId);
+      systemStateUtil.waitForState(deviceId, DeviceConnectionState.DEVICE, Duration.ofMinutes(15));
+      systemStateUtil.waitUntilReady(deviceId, deviceReadyTimeout);
+      // Device will be unrooted after rebooting.
+      unused = systemStateUtil.becomeRoot(deviceId);
+      systemStateUtil.waitUntilReady(deviceId, Duration.ofMinutes(3));
+    }
+
+    var unusedString = systemSettingUtil.allowMockLocation(deviceId);
+    systemStateUtil.waitUntilReady(deviceId, Duration.ofMinutes(3));
+    if (!isTestHarnessModeEnabled) {
+      if (!DeviceUtil.inSharedLab()) {
+        systemSettingUtil.keepAwake(deviceId, /* alwaysAwake= */ true);
+      } else {
+        logger.atSevere().log(
+            "Ignoring attempt to keep awake device %s while not managing devices.", deviceId);
+      }
+      systemStateUtil.waitUntilReady(deviceId, Duration.ofMinutes(3));
+    }
+
+    disableScreenLock(deviceId);
+
+    // Try to enable unknown sources option.
+    enableUnknownSources(deviceId);
+
+    disablePackageVerifier(deviceId);
+    logger.atInfo().log("waiting for %s ready", deviceId);
+    systemStateUtil.waitUntilReady(deviceId, Duration.ofMinutes(3));
+  }
+
+  private void disableScreenLock(String deviceId)
+      throws MobileHarnessException, InterruptedException {
+    Set<String> features = systemSpecUtil.getSystemFeatures(deviceId);
+    // ScreenLock not used on Android Automotive devices due to b/135046763#comment51
+    if (!features.contains(FEATURE_EMBEDDED) && !features.contains(FEATURE_AUTOMOTIVE)) {
+      // Try to disable screen lock.
+      logger.atInfo().log("Disable screen lock");
+      systemSettingUtil.disableScreenLock(
+          deviceId, systemSettingUtil.getDeviceSdkVersion(deviceId));
+      // Due to b/30934672, sleep 10ms waiting for soft reboot.
+      sleeper.sleep(Duration.ofMillis(10));
+    }
+  }
+
+  private void enableUnknownSources(String serial)
+      throws MobileHarnessException, InterruptedException {
+    if (!DeviceUtil.inSharedLab()) {
+      systemSettingUtil.enableUnknownSources(serial, systemSettingUtil.getDeviceSdkVersion(serial));
+    } else {
+      logger.atSevere().log(
+          "Ignoring attempt to enable unknown sources on device %s"
+              + " while not managing devices.",
+          serial);
+    }
+  }
+
+  private void disablePackageVerifier(String serial)
+      throws MobileHarnessException, InterruptedException {
+    int sdkVersion;
+    try {
+      sdkVersion = systemSettingUtil.getDeviceSdkVersion(serial);
+    } catch (MobileHarnessException e) {
+      logger.atWarning().withCause(e).log(
+          "Failed to get SDK version before disabling package verifier option, aborted.");
+      return;
+    }
+
+    if (!DeviceUtil.inSharedLab()) {
+      packageManagerUtil.disablePackageVerifier(serial, sdkVersion);
+    } else {
+      logger.atSevere().log(
+          "Ignoring attempt to disable package verifier on device %s"
+              + " while not managing devices.",
+          serial);
+    }
+  }
+}
