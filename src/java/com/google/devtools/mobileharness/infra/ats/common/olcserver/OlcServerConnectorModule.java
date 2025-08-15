ad0acefeb2529883c54edd918de24399a22133ee
Shian Hou <shian@google.com>
2025-08-15 02:46:24 -0700

    Internal change
    PiperOrigin-RevId: 795406005

--- src/java/com/google/devtools/mobileharness/infra/ats/common/olcserver/OlcServerConnectorModule.java
+++ src/java/com/google/devtools/mobileharness/infra/ats/common/olcserver/OlcServerConnectorModule.java
@@ -20,9 +20,19 @@ import com.google.devtools.mobileharness.infra.ats.common.FlagsString;
 import com.google.devtools.mobileharness.infra.ats.common.olcserver.Annotations.ClientComponentName;
 import com.google.devtools.mobileharness.infra.ats.common.olcserver.Annotations.ClientId;
 import com.google.devtools.mobileharness.infra.ats.common.olcserver.Annotations.DeviceInfraServiceFlags;
+import com.google.devtools.mobileharness.infra.ats.common.olcserver.Annotations.ServerChannel;
+import com.google.devtools.mobileharness.infra.ats.common.olcserver.Annotations.ServerStub;
+import com.google.devtools.mobileharness.infra.client.longrunningservice.rpc.stub.ControlStub;
+import com.google.devtools.mobileharness.infra.client.longrunningservice.rpc.stub.SessionStub;
+import com.google.devtools.mobileharness.infra.client.longrunningservice.rpc.stub.VersionStub;
+import com.google.devtools.mobileharness.infra.client.longrunningservice.rpc.stub.grpc.ControlGrpcStub;
+import com.google.devtools.mobileharness.infra.client.longrunningservice.rpc.stub.grpc.SessionGrpcStub;
+import com.google.devtools.mobileharness.infra.client.longrunningservice.rpc.stub.grpc.VersionGrpcStub;
 import com.google.devtools.mobileharness.shared.util.flags.Flags;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import io.grpc.Channel;
+import javax.inject.Singleton;
 
 /** Module for ATS console /local runner that connect to ATS OLC server. */
 public class OlcServerConnectorModule extends AbstractModule {
@@ -41,7 +51,7 @@ public class OlcServerConnectorModule extends AbstractModule {
   @Override
   protected void configure() {
     if (!Flags.instance().atsConsoleOlcServerEmbeddedMode.getNonNull()) {
-      install(new OlcServerGrpcStubModule());
+      install(new OlcServerGrpcChannelModule());
     }
   }
 
@@ -61,5 +71,26 @@ public class OlcServerConnectorModule extends AbstractModule {
   @DeviceInfraServiceFlags
   FlagsString provideDeviceInfraServiceFlags() {
     return deviceInfraServiceFlags;
+  }
+
+  @Provides
+  @Singleton
+  @ServerStub(ServerStub.Type.CONTROL_SERVICE)
+  ControlStub provideServerControlStub(@ServerChannel Channel serverChannel) {
+    return new ControlGrpcStub(serverChannel);
+  }
+
+  @Provides
+  @Singleton
+  @ServerStub(ServerStub.Type.SESSION_SERVICE)
+  SessionStub provideServerSessionStub(@ServerChannel Channel serverChannel) {
+    return new SessionGrpcStub(serverChannel);
+  }
+
+  @Provides
+  @Singleton
+  @ServerStub(ServerStub.Type.VERSION_SERVICE)
+  VersionStub provideServerVersionStub(@ServerChannel Channel serverChannel) {
+    return new VersionGrpcStub(serverChannel);
   }
 }
