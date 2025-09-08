0e955e1278b38119bd57500b6b794586e21c7fe1
Baisheng Zheng <baisheng@google.com>
2025-09-08 01:09:39 -0700

    Internal change
    PiperOrigin-RevId: 804312017

--- src/java/com/google/devtools/mobileharness/shared/version/Version.java
+++ src/java/com/google/devtools/mobileharness/shared/version/Version.java
@@ -28,7 +28,7 @@ public final class Version implements Comparable<Version> {
 
   public static final Version MIN_CLIENT_VERSION = new Version(4, 29, 0);
 
-  public static final Version LAB_VERSION = new Version(4, 334, 0);
+  public static final Version LAB_VERSION = new Version(4, 335, 0);
 
   public static final Version MIN_LAB_VERSION = new Version(4, 126, 0);
 
