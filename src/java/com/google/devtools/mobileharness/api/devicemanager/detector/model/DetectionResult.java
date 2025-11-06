20814a90aa6d8b96ee7abc333f8f9d257e8576dd
DeviceInfra <no-reply@google.com>
2025-11-05 18:47:27 -0800

    Internal change
    PiperOrigin-RevId: 828722499

--- src/java/com/google/devtools/mobileharness/api/devicemanager/detector/model/DetectionResult.java
+++ src/java/com/google/devtools/mobileharness/api/devicemanager/detector/model/DetectionResult.java
@@ -44,7 +44,7 @@ public abstract class DetectionResult {
 
   /** The detection type enum corresponding to each detector. */
   public enum DetectionType {
-    ALUMINIUM_EXECUTOR,
+    ANDROID_DESKTOP_EXECUTOR,
     AAOS_HYPERVISOR,
     ADB,
     CROS,
