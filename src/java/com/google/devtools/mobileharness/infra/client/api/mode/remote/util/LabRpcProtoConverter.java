acad8752e19d9d79fbe6ea04928eebe06e0c9955
DeviceInfra <no-reply@google.com>
2025-11-19 22:41:46 -0800

    Internal change
    PiperOrigin-RevId: 834600965

--- src/java/com/google/devtools/mobileharness/infra/client/api/mode/remote/util/LabRpcProtoConverter.java
+++ src/java/com/google/devtools/mobileharness/infra/client/api/mode/remote/util/LabRpcProtoConverter.java
@@ -111,10 +111,6 @@ public class LabRpcProtoConverter {
       @Nullable TestMessageManager testMessageManager,
       @Nullable TestMessagePoster testMessagePoster)
       throws MobileHarnessException {
-    // TODO: Remove this log after the bug is fixed.
-    logger.atInfo().log(
-        "GetTestStatusResponse test result type: %s, test result: %s, test result cause: %s",
-        resp.getTestResultType(), resp.getTestResult(), resp.getTestResultCause());
     String testId = testInfo.locator().getId();
     String remoteLog = resp.getTestLog();
     testInfo.log().append(remoteLog);
