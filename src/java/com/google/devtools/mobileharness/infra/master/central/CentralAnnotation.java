e72ce228f2b4a698e7cf5df70cac7027e1332bde
Jianxin Gao <jianxingao@google.com>
2025-12-03 23:53:26 -0800

    Internal change
    PiperOrigin-RevId: 840098653

---
+++ src/java/com/google/devtools/mobileharness/infra/master/central/CentralAnnotation.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.mobileharness.infra.master.central;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import javax.inject.Qualifier;
+
+/** Annotations used in Central module of Mobile Harness Master. */
+public final class CentralAnnotation {
+
+  /**
+   * Annotation used to identify whether to enable the in-memory cache of {@link JobInfoCentral} &
+   * {@link LabInfoCentral}.
+   */
+  @Qualifier
+  @Retention(RetentionPolicy.RUNTIME)
+  public @interface EnableInfoCache {}
+
+  private CentralAnnotation() {}
+}
