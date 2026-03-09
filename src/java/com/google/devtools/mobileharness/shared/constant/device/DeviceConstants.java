46852d7acdf3ca94449920402a296de472b39787
DeviceInfra <no-reply@google.com>
2026-03-09 01:07:09 -0700

    Internal change
    PiperOrigin-RevId: 880711213

--- src/java/com/google/devtools/mobileharness/shared/constant/device/DeviceConstants.java
+++ src/java/com/google/devtools/mobileharness/shared/constant/device/DeviceConstants.java
@@ -28,6 +28,7 @@ public final class DeviceConstants {
           "AndroidFastbootDevice",
           "AndroidFastbootdModeDevice",
           "AndroidOfflineDevice",
+          "AndroidRecoveryDevice",
           "AndroidUnauthorizedDevice",
           "DisconnectedDevice",
           "FailedDevice");
