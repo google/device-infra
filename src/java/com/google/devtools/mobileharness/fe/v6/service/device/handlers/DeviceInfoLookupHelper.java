cd63a47bd1198865ef2ff9687767aa684d63d9c6
DeviceInfra <no-reply@google.com>
2026-03-27 03:15:35 -0700

    Internal change
    PiperOrigin-RevId: 890346867

---
+++ src/java/com/google/devtools/mobileharness/fe/v6/service/device/handlers/DeviceInfoLookupHelper.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.mobileharness.fe.v6.service.device.handlers;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.devtools.mobileharness.api.query.proto.FilterProto.DeviceFilter;
+import com.google.devtools.mobileharness.api.query.proto.FilterProto.DeviceFilter.DeviceMatchCondition;
+import com.google.devtools.mobileharness.api.query.proto.FilterProto.DeviceFilter.DeviceMatchCondition.DeviceUuidMatchCondition;
+import com.google.devtools.mobileharness.api.query.proto.FilterProto.StringMatchCondition;
+import com.google.devtools.mobileharness.api.query.proto.LabQueryProto.DeviceInfo;
+import com.google.devtools.mobileharness.api.query.proto.LabQueryProto.LabQuery;
+import com.google.devtools.mobileharness.fe.v6.service.shared.providers.LabInfoProvider;
+import com.google.devtools.mobileharness.shared.labinfo.proto.LabInfoServiceProto.GetLabInfoRequest;
+import com.google.devtools.mobileharness.shared.labinfo.proto.LabInfoServiceProto.GetLabInfoResponse;
+import java.util.concurrent.Executor;
+
+/** Helper for looking up device information. */
+public final class DeviceInfoLookupHelper {
+
+  private DeviceInfoLookupHelper() {}
+
+  /**
+   * Looks up the {@link DeviceInfo} for a single device ID.
+   *
+   * @return a future that resolves to the DeviceInfo
+   * @throws RuntimeException if the device is not found during future transformation
+   */
+  public static ListenableFuture<DeviceInfo> lookUpDeviceInfoAsync(
+      LabInfoProvider labInfoProvider, String deviceId, String universe, Executor executor) {
+    GetLabInfoRequest labInfoRequest = createSingleDeviceLabInfoRequest(deviceId);
+    return Futures.transform(
+        labInfoProvider.getLabInfoAsync(labInfoRequest, universe),
+        response -> getDeviceInfoFromResponse(response, deviceId, universe),
+        executor);
+  }
+
+  private static GetLabInfoRequest createSingleDeviceLabInfoRequest(String deviceId) {
+    return GetLabInfoRequest.newBuilder()
+        .setLabQuery(
+            LabQuery.newBuilder()
+                .setFilter(
+                    LabQuery.Filter.newBuilder()
+                        .setDeviceFilter(
+                            DeviceFilter.newBuilder()
+                                .addDeviceMatchCondition(
+                                    DeviceMatchCondition.newBuilder()
+                                        .setDeviceUuidMatchCondition(
+                                            DeviceUuidMatchCondition.newBuilder()
+                                                .setCondition(
+                                                    StringMatchCondition.newBuilder()
+                                                        .setInclude(
+                                                            StringMatchCondition.Include
+                                                                .newBuilder()
+                                                                .addExpected(deviceId)))))))
+                .setDeviceViewRequest(LabQuery.DeviceViewRequest.getDefaultInstance()))
+        .build();
+  }
+
+  private static DeviceInfo getDeviceInfoFromResponse(
+      GetLabInfoResponse response, String deviceId, String universe) {
+    return response
+        .getLabQueryResult()
+        .getDeviceView()
+        .getGroupedDevices()
+        .getDeviceList()
+        .getDeviceInfoList()
+        .stream()
+        .findFirst()
+        .orElseThrow(
+            () ->
+                new RuntimeException(
+                    "Device not found: " + deviceId + " in universe: " + universe));
+  }
+}
---
+++ src/javatests/com/google/devtools/mobileharness/fe/v6/service/device/handlers/DeviceInfoLookupHelperTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.mobileharness.fe.v6.service.device.handlers;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
+import static com.google.common.util.concurrent.Futures.immediateFuture;
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.devtools.mobileharness.api.query.proto.LabQueryProto.DeviceInfo;
+import com.google.devtools.mobileharness.api.query.proto.LabQueryProto.DeviceList;
+import com.google.devtools.mobileharness.api.query.proto.LabQueryProto.GroupedDevices;
+import com.google.devtools.mobileharness.api.query.proto.LabQueryProto.LabQueryResult;
+import com.google.devtools.mobileharness.api.query.proto.LabQueryProto.LabQueryResult.DeviceView;
+import com.google.devtools.mobileharness.fe.v6.service.shared.providers.LabInfoProvider;
+import com.google.devtools.mobileharness.shared.labinfo.proto.LabInfoServiceProto.GetLabInfoRequest;
+import com.google.devtools.mobileharness.shared.labinfo.proto.LabInfoServiceProto.GetLabInfoResponse;
+import java.util.concurrent.ExecutionException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+@RunWith(JUnit4.class)
+public final class DeviceInfoLookupHelperTest {
+
+  private static final String DEVICE_ID = "test_device_id";
+  private static final String UNIVERSE = "test_universe";
+
+  @Rule public final MockitoRule mocks = MockitoJUnit.rule();
+
+  @Mock private LabInfoProvider labInfoProvider;
+  @Captor private ArgumentCaptor<GetLabInfoRequest> labInfoRequestCaptor;
+
+  @Test
+  public void lookUpDeviceInfoAsync_success() throws Exception {
+    DeviceInfo expectedDeviceInfo = DeviceInfo.getDefaultInstance();
+    GetLabInfoResponse labInfoResponse =
+        GetLabInfoResponse.newBuilder()
+            .setLabQueryResult(
+                LabQueryResult.newBuilder()
+                    .setDeviceView(
+                        DeviceView.newBuilder()
+                            .setGroupedDevices(
+                                GroupedDevices.newBuilder()
+                                    .setDeviceList(
+                                        DeviceList.newBuilder()
+                                            .addDeviceInfo(expectedDeviceInfo)))))
+            .build();
+    when(labInfoProvider.getLabInfoAsync(any(GetLabInfoRequest.class), eq(UNIVERSE)))
+        .thenReturn(immediateFuture(labInfoResponse));
+
+    ListenableFuture<DeviceInfo> deviceInfoFuture =
+        DeviceInfoLookupHelper.lookUpDeviceInfoAsync(
+            labInfoProvider, DEVICE_ID, UNIVERSE, directExecutor());
+
+    assertThat(deviceInfoFuture.get()).isEqualTo(expectedDeviceInfo);
+    verify(labInfoProvider).getLabInfoAsync(labInfoRequestCaptor.capture(), eq(UNIVERSE));
+    GetLabInfoRequest capturedRequest = labInfoRequestCaptor.getValue();
+    // Verifies that deviceViewRequest is set in the request. This is critical because without it,
+    // the backend will not return the DeviceView in the response, and then
+    // getDeviceInfoFromResponse() will find nothing.
+    assertThat(capturedRequest.getLabQuery().hasDeviceViewRequest()).isTrue();
+    assertThat(capturedRequest.getLabQuery().getDeviceViewRequest()).isEqualToDefaultInstance();
+  }
+
+  @Test
+  public void lookUpDeviceInfoAsync_notFound() throws Exception {
+    GetLabInfoResponse labInfoResponse =
+        GetLabInfoResponse.newBuilder()
+            .setLabQueryResult(
+                LabQueryResult.newBuilder().setDeviceView(DeviceView.getDefaultInstance()))
+            .build();
+    when(labInfoProvider.getLabInfoAsync(any(GetLabInfoRequest.class), eq(UNIVERSE)))
+        .thenReturn(immediateFuture(labInfoResponse));
+
+    ListenableFuture<DeviceInfo> deviceInfoFuture =
+        DeviceInfoLookupHelper.lookUpDeviceInfoAsync(
+            labInfoProvider, DEVICE_ID, UNIVERSE, directExecutor());
+
+    ExecutionException exception = assertThrows(ExecutionException.class, deviceInfoFuture::get);
+    assertThat(exception).hasCauseThat().isInstanceOf(RuntimeException.class);
+    assertThat(exception).hasCauseThat().hasMessageThat().contains("Device not found");
+  }
+}
