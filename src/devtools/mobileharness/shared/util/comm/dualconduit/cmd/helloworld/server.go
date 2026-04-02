259f1f10bf4c2499d35429fdcc28972b64ebb563
Yuecheng Zhu <zhuyuecheng@google.com>
2026-04-02 06:17:00 -0700

    Internal change
    PiperOrigin-RevId: 893471621

--- src/devtools/mobileharness/shared/util/comm/dualconduit/cmd/helloworld/server.go
+++ src/devtools/mobileharness/shared/util/comm/dualconduit/cmd/helloworld/server.go
@@ -2,7 +2,6 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"log"
@@ -11,22 +10,13 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
-	helloworldpb "github.com/google/device-infra/src/devtools/mobileharness/shared/util/comm/dualconduit/tests/helloworld/proto/helloworldpb"
+	"github.com/google/device-infra/src/devtools/mobileharness/shared/util/comm/dualconduit/tests/helloworld"
 	helloworldsvcpb "github.com/google/device-infra/src/devtools/mobileharness/shared/util/comm/dualconduit/tests/helloworld/proto/helloworldsvcpb"
 )
 
 var (
 	port = flag.Int("port", 50052, "The server port")
 )
-
-type server struct {
-	helloworldsvcpb.UnimplementedGreeterServer
-}
-
-func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
-	log.Printf("Received SayHello: %v", in.GetName())
-	return &helloworldpb.HelloReply{Message: "Hello " + in.GetName()}, nil
-}
 
 func main() {
 	flag.Parse()
@@ -35,7 +25,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	helloworldsvcpb.RegisterGreeterServer(s, &server{})
+	helloworldsvcpb.RegisterGreeterServer(s, &helloworld.Server{})
 	reflection.Register(s)
 	log.Printf("helloworld server listening on %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
