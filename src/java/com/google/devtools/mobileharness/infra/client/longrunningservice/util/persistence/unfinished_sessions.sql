e301f7fe70c4e8b0d9357b1e808c4f3d645b4eed
OmniLab Team <deviceinfra-copybara@google.com>
2026-06-01 20:25:01 -0700

    Internal change
    PiperOrigin-RevId: 925069940

--- src/java/com/google/devtools/mobileharness/infra/client/longrunningservice/util/persistence/unfinished_sessions.sql
+++ src/java/com/google/devtools/mobileharness/infra/client/longrunningservice/util/persistence/unfinished_sessions.sql
@@ -2,3 +2,8 @@ CREATE TABLE IF NOT EXISTS unfinished_sessions (
   session_id VARCHAR(64) PRIMARY KEY,
   session_data MEDIUMBLOB NOT NULL -- binary of mobileharness.infra.client.longrunningservice.SessionPersistenceData
 ) CHARACTER SET utf8mb4;
+
+-- Safely migrate existing VARBINARY columns to MEDIUMBLOB in older installations.
+-- Modifying a column to its current type is a seamless no-op in MySQL.
+ALTER TABLE unfinished_sessions MODIFY COLUMN session_data MEDIUMBLOB NOT NULL;
+
