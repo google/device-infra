a8ed2edf64906edb186086be1a6f846c9e67caf9
Shian Hou <shian@google.com>
2026-07-12 06:48:26 -0700

    Internal change
    PiperOrigin-RevId: 946564066

--- src/javatests/com/google/wireless/qa/mobileharness/shared/api/decorator/base/LifecycleDecoratorTest.java
+++ src/javatests/com/google/wireless/qa/mobileharness/shared/api/decorator/base/LifecycleDecoratorTest.java
@@ -16,16 +16,22 @@
 
 package com.google.wireless.qa.mobileharness.shared.api.decorator.base;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
 
 import com.google.devtools.mobileharness.api.model.error.BasicErrorId;
 import com.google.devtools.mobileharness.api.model.error.InfraErrorId;
 import com.google.devtools.mobileharness.api.model.error.MobileHarnessException;
+import com.google.devtools.mobileharness.shared.util.error.MoreThrowables;
+import com.google.devtools.mobileharness.shared.util.junit.rule.CaptureLogs;
 import com.google.wireless.qa.mobileharness.shared.api.device.Device;
 import com.google.wireless.qa.mobileharness.shared.api.driver.Driver;
 import com.google.wireless.qa.mobileharness.shared.model.job.TestInfo;
@@ -46,72 +52,130 @@ public final class LifecycleDecoratorTest {
 
   @Rule public final MockitoRule mockito = MockitoJUnit.rule();
 
+  @Rule
+  public final CaptureLogs captureLogs = new CaptureLogs(LifecycleDecorator.class.getName(), true);
+
   @Mock private Driver decorated;
   @Mock private TestInfo testInfo;
   @Mock private Device device;
 
-  private TestLifecycleDecorator decorator;
-
-  private static class TestLifecycleDecorator extends LifecycleDecorator {
-    private TestLifecycleDecorator(Driver decorated, TestInfo testInfo) {
-      super(decorated, testInfo);
-    }
-
-    @Override
-    protected void setUp(TestInfo testInfo) throws MobileHarnessException, InterruptedException {}
-
-    @Override
-    protected void tearDown(TestInfo testInfo)
-        throws MobileHarnessException, InterruptedException {}
-  }
+  private LifecycleDecorator decorator;
+  private String logPrefix;
 
   @Before
   public void setUp() {
     when(decorated.getDevice()).thenReturn(device);
     when(testInfo.log()).thenReturn(new Log(new Timing()));
-    decorator = spy(new TestLifecycleDecorator(decorated, testInfo));
+    decorator =
+        mock(
+            LifecycleDecorator.class,
+            withSettings().useConstructor(decorated, testInfo).defaultAnswer(CALLS_REAL_METHODS));
+    logPrefix = "Decorator [" + decorator.getClass().getSimpleName() + "] ";
   }
 
   @Test
-  public void run_normalExecution_executesInOrder() throws Exception {
+  public void run_normalExecution_executesInOrderAndLogs() throws Exception {
     decorator.run(testInfo);
 
     InOrder inOrder = inOrder(decorator, decorated);
     inOrder.verify(decorator).setUp(testInfo);
     inOrder.verify(decorated).run(testInfo);
     inOrder.verify(decorator).tearDown(testInfo);
+
+    assertThat(captureLogs.getLogs()).contains(logPrefix + "setup starting.");
+    assertThat(captureLogs.getLogs()).contains(logPrefix + "setup finished.");
+    assertThat(captureLogs.getLogs()).contains(logPrefix + "teardown starting.");
+    assertThat(captureLogs.getLogs()).contains(logPrefix + "teardown finished.");
   }
 
   @Test
-  public void run_innerDriverThrowsException_runsTeardownAndBubblesException() throws Exception {
+  public void run_innerDriverThrowsException_runsTeardownAndBubblesExceptionAndLogs()
+      throws Exception {
     MobileHarnessException exception =
         new MobileHarnessException(BasicErrorId.JOB_TIMEOUT, "Test failed");
     doThrow(exception).when(decorated).run(testInfo);
 
-    try {
-      decorator.run(testInfo);
-    } catch (MobileHarnessException e) {
-      // Expected
-    }
+    MobileHarnessException thrown =
+        assertThrows(MobileHarnessException.class, () -> decorator.run(testInfo));
+    assertThat(thrown).isSameInstanceAs(exception);
 
     verify(decorator).setUp(testInfo);
     verify(decorator).tearDown(testInfo);
+
+    assertThat(captureLogs.getLogs()).contains(logPrefix + "setup finished.");
+    assertThat(captureLogs.getLogs()).contains(logPrefix + "teardown finished.");
   }
 
   @Test
-  public void run_setupThrowsException_doesNotRunDriverOrTeardown() throws Exception {
+  public void run_setupThrowsException_doesNotRunDriverOrTeardownAndLogsFailure() throws Exception {
     MobileHarnessException exception =
         new MobileHarnessException(
             InfraErrorId.CLIENT_LOCAL_MODE_ALLOCATED_DEVICE_NOT_FOUND, "Setup failed");
     doThrow(exception).when(decorator).setUp(testInfo);
 
-    try {
-      decorator.run(testInfo);
-    } catch (MobileHarnessException e) {
-      // Expected
-    }
+    MobileHarnessException thrown =
+        assertThrows(MobileHarnessException.class, () -> decorator.run(testInfo));
+    assertThat(thrown).isSameInstanceAs(exception);
 
     verify(decorated, never()).run(testInfo);
     verify(decorator, never()).tearDown(testInfo);
+
+    assertThat(captureLogs.getLogs()).contains(logPrefix + "setup starting.");
+    assertThat(captureLogs.getLogs())
+        .contains(
+            logPrefix
+                + "setup finished with failure ["
+                + MoreThrowables.shortDebugString(exception)
+                + "]");
+  }
+
+  @Test
+  public void run_teardownThrowsException_bubblesExceptionAndLogsFailure() throws Exception {
+    MobileHarnessException exception =
+        new MobileHarnessException(BasicErrorId.JOB_TIMEOUT, "Teardown failed");
+    doThrow(exception).when(decorator).tearDown(testInfo);
+
+    MobileHarnessException thrown =
+        assertThrows(MobileHarnessException.class, () -> decorator.run(testInfo));
+    assertThat(thrown).isSameInstanceAs(exception);
+
+    verify(decorator).setUp(testInfo);
+    verify(decorated).run(testInfo);
+    verify(decorator).tearDown(testInfo);
+
+    assertThat(captureLogs.getLogs()).contains(logPrefix + "teardown starting.");
+    assertThat(captureLogs.getLogs())
+        .contains(
+            logPrefix
+                + "teardown finished with failure ["
+                + MoreThrowables.shortDebugString(exception)
+                + "]");
+  }
+
+  @Test
+  public void run_driverAndTeardownThrowException_suppressesTeardownExceptionAndLogsFailure()
+      throws Exception {
+    MobileHarnessException runException =
+        new MobileHarnessException(BasicErrorId.JOB_TIMEOUT, "Run failed");
+    MobileHarnessException teardownException =
+        new MobileHarnessException(BasicErrorId.JOB_TIMEOUT, "Teardown failed");
+    doThrow(runException).when(decorated).run(testInfo);
+    doThrow(teardownException).when(decorator).tearDown(testInfo);
+
+    MobileHarnessException thrown =
+        assertThrows(MobileHarnessException.class, () -> decorator.run(testInfo));
+    assertThat(thrown).isSameInstanceAs(runException);
+    assertThat(thrown.getSuppressed()).asList().containsExactly(teardownException);
+
+    verify(decorator).setUp(testInfo);
+    verify(decorated).run(testInfo);
+    verify(decorator).tearDown(testInfo);
+
+    assertThat(captureLogs.getLogs())
+        .contains(
+            logPrefix
+                + "teardown finished with failure ["
+                + MoreThrowables.shortDebugString(teardownException)
+                + "]");
   }
 }
