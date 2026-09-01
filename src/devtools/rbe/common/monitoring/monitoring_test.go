39c3e0bb7588e13c460578d382ccab5075cdccc5
OmniLab Team <deviceinfra-copybara@google.com>
2026-09-01 10:55:41 -0700

    Internal change
    PiperOrigin-RevId: 974589618

--- src/devtools/rbe/common/monitoring/monitoring_test.go
+++ src/devtools/rbe/common/monitoring/monitoring_test.go
@@ -51,41 +51,65 @@ func TestMonitoringWorkflows(t *testing.T) {
 	// Test CASProxy metrics APIs
 	Init("casproxy")
 
+	testMu.Lock()
 	lastRecordedMethod = ""
+	testMu.Unlock()
 	RecordCacheRequest("ByteStream.Read", true)
+	testMu.Lock()
 	if lastRecordedMethod != "ByteStream.Read" {
 		t.Errorf("RecordCacheRequest: lastRecordedMethod = %q, want %q", lastRecordedMethod, "ByteStream.Read")
 	}
+	testMu.Unlock()
 
+	testMu.Lock()
 	lastRecordedServedBytes = 0
+	testMu.Unlock()
 	RecordServedBytes("local_cache", 2048)
+	testMu.Lock()
 	if lastRecordedServedBytes != 2048 {
 		t.Errorf("RecordServedBytes: lastRecordedServedBytes = %d, want 2048", lastRecordedServedBytes)
 	}
+	testMu.Unlock()
 
+	testMu.Lock()
 	lastRecordedWANBytes = 0
+	testMu.Unlock()
 	RecordWANBytes("ByteStream.Read", 4096)
+	testMu.Lock()
 	if lastRecordedWANBytes != 4096 {
 		t.Errorf("RecordWANBytes: lastRecordedWANBytes = %d, want 4096", lastRecordedWANBytes)
 	}
+	testMu.Unlock()
 
+	testMu.Lock()
 	lastRecordedTotal = 0
+	testMu.Unlock()
 	RecordStorageUsage(100*1024*1024*1024, 25*1024*1024*1024)
+	testMu.Lock()
 	if lastRecordedTotal != 100*1024*1024*1024 {
 		t.Errorf("RecordStorageUsage: lastRecordedTotal = %d, want %d", lastRecordedTotal, int64(100*1024*1024*1024))
 	}
+	testMu.Unlock()
 
+	testMu.Lock()
 	lastRecordedStatus = ""
+	testMu.Unlock()
 	RecordEvictionRun("evicted", 1024*1024, 5, 25*time.Millisecond)
+	testMu.Lock()
 	if lastRecordedStatus != "evicted" {
 		t.Errorf("RecordEvictionRun: lastRecordedStatus = %q, want %q", lastRecordedStatus, "evicted")
 	}
+	testMu.Unlock()
 
+	testMu.Lock()
 	lastRecordedUpstreamRPC = ""
+	testMu.Unlock()
 	RecordUpstreamRPC("CAS.BatchReadBlobs", "NOT_FOUND", 120*time.Millisecond)
+	testMu.Lock()
 	if lastRecordedUpstreamRPC != "CAS.BatchReadBlobs" {
 		t.Errorf("RecordUpstreamRPC: lastRecordedUpstreamRPC = %q, want %q", lastRecordedUpstreamRPC, "CAS.BatchReadBlobs")
 	}
+	testMu.Unlock()
 
 	// Signal shutdown to verify flushing logic does not panic or deadlock
 	Shutdown()
