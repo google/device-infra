3895740a38c420253a84971b34d586de607f35bc
OmniLab Team <deviceinfra-copybara@google.com>
2026-09-03 07:10:40 -0700

    Internal change
    PiperOrigin-RevId: 975710673

--- src/java/com/google/wireless/qa/mobileharness/shared/api/spec/CrosDecoratorSpec.java
+++ src/java/com/google/wireless/qa/mobileharness/shared/api/spec/CrosDecoratorSpec.java
@@ -32,6 +32,13 @@ public interface CrosDecoratorSpec {
   String LSNEXUS_CIPD_PATH = CIPD_DIR + "lsnexus";
 
   // Inventory service related constants.
+  /** Parameter name for the endpoint (host:port) of the inventory service. */
+  @ParamAnnotation(help = "Endpoint (host:port) of the inventory service.", required = false)
+  String INVENTORY_SERVICE = "inventory_service";
+
+  /** The default endpoint for the inventory service. */
+  String DEFAULT_INVENTORY_SERVICE = "localhost:1485";
+
   /** Parameter name for the hostname or IP address of the inventory service. */
   @ParamAnnotation(help = "Hostname or IP address of the inventory service.", required = false)
   String INVENTORY_SERVICE_HOST = "inventory_service_host";
