96be80bbb056761c967edba9bcdbbcf7670586df
OmniLab Team <deviceinfra-copybara@google.com>
2026-09-21 05:18:59 -0700

    Internal change
    PiperOrigin-RevId: 985207468

--- src/java/com/google/devtools/deviceinfra/ext/devicemanagement/device/platform/android/AndroidDeviceDelegate.java
+++ src/java/com/google/devtools/deviceinfra/ext/devicemanagement/device/platform/android/AndroidDeviceDelegate.java
@@ -484,6 +484,7 @@ public abstract class AndroidDeviceDelegate {
     device.addSupportedDriver("NoOpDriver");
     device.addSupportedDriver("PlaywrightWebDriver");
     device.addSupportedDriver("ProtractorWebDriver");
+    device.addSupportedDriver("SeleniumWebDriver");
     device.addSupportedDriver("SlateDriver");
     device.addSupportedDriver("TradefedTest");
     // keep-sorted end
